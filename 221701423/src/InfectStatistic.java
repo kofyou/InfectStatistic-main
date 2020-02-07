7d10d8b1c8b2b33f289d53584a1fc7cf4ff43362
Xily9 <572089608@qq.com>
2020-02-07 23:55:48 +0800

    读取分析日志文件内容

--- 221701423/src/InfectStatistic.java
+++ 221701423/src/InfectStatistic.java
@@ -8,6 +8,38 @@
  */
 class InfectStatistic {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+
     }
+
+    public static class Log {
+        private String province;
+        private String type;
+        private int count;
+
+        public String getProvince() {
+            return province;
+        }
+
+        public void setProvince(String province) {
+            this.province = province;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        public void setCount(int count) {
+            this.count = count;
+        }
+    }
+
+
 }

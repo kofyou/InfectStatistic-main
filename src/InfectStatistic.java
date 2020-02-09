28fd8878d07e36a5745fb47298f505649f3b85b0
Kylinseph <52260506@qq.com>
2020-02-09 16:31:14 +0800

    chore: create project and test commit
    Signed-off-by: Kylin <52260506@qq.com>

---
+++ src/InfectStatistic.java
@@ -0,0 +1,15 @@
+/**
+ * InfectStatistic
+ * TODO
+ *
+ * @author PSF
+ * @email 52260506@qq.com
+ * @version 1.0
+ * @since 2020/2/9
+ */
+class InfectStatistic {
+    public static void main(String[] args) {
+
+        System.out.println("helloworld");
+    }
+}

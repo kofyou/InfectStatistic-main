e27584c5be2b9c119707143384f36234c90d37ad
bzzd2333 <59527825+bzzd2333@users.noreply.github.com>
2020-02-10 09:53:55 +0800

    第三次commit
    实现了感染患者流入、疑似患者流入和确诊的计算；实现了全国情况的统计。
    至此日志处理部分完成。

--- 221701236/src/InfectStatistic.java
+++ 221701236/src/InfectStatistic.java
@@ -27,6 +27,8 @@ class InfectStatistic
 	private List<String> name;
 	//存放省份名称与其具体情况的映射
 	private HashMap<String,Province> map;
+	//存放全国的具体情况
+	private Province country;
 		
 	//构造函数
 	public InfectStatistic(String[] args)
@@ -36,6 +38,7 @@ class InfectStatistic
 		logPath = "G:/log/";
 		name = new ArrayList<>();
 		map = new HashMap<String,Province>();
+		country = new Province("全国");
 		this.init();
 	}
 		
@@ -73,6 +76,8 @@ class InfectStatistic
 	        	//System.out.println(logDate);	                    
 	        }            	                
 	    }
+	    //统计全国的情况
+	    allStatistic();
 	}
 		
 	//处理单行
@@ -103,8 +108,27 @@ class InfectStatistic
 		    	}
 		        break;
 		    case "感染患者":
+		    	//流出省份减少感染患者
+		    	map.get(array[0]).removeIp(array[4]);
+		    	//流入省份增加感染患者
+		    	map.get(array[3]).addIp(array[4]);
 		        break;
 		    case "疑似患者":
+		    	if(array[2].equals("流入"))
+		    	{
+		    		//流出省份减少疑似患者
+			    	map.get(array[0]).removeSp(array[4]);
+			    	//流入省份增加疑似患者
+			    	map.get(array[3]).addSp(array[4]);
+		    	}
+		    	//疑似患者确诊感染
+		    	else
+		    	{
+		    		//感染患者增加
+		    		map.get(array[0]).addIp(array[3]);
+		    		//疑似患者减少
+		    		map.get(array[0]).removeSp(array[3]);
+		    	}
 		        break;
 		    case "死亡":
 		    	map.get(array[0]).dead(array[2]);
@@ -113,19 +137,29 @@ class InfectStatistic
 		    	map.get(array[0]).cure(array[2]);
 		        break;
 		    case "排除":
-		    	map.get(array[0]).remove(array[3]);
+		    	map.get(array[0]).removeSp(array[3]);
 		    	break;
 		    default:
 		    	break;
 		}	
 
 	}
+	
+	//统计全国的情况
+	public void allStatistic()
+	{
+		for(int i=0;i<name.size();i++)
+		{
+			country.allAdd(map.get(name.get(i)));
+		}
+	}
 
 	//生成output.txt文件
 	public void output()
 	{
+		country.output();
 		for(int i=0;i<name.size();i++)
-		{
+		{			
 			//System.out.println(i+":"+name.get(i));
 			map.get(name.get(i)).output();
 		}
@@ -186,37 +220,51 @@ class Province
 	{
 		name = n;
 	}
-	//新增疑似患者
+	//疑似患者增加
 	public void addSp(String str)
 	{
 		str = str.substring(0,str.length()-1);
 		suspectedPatients += Integer.parseInt(str);
 	}
-	//新增疑似患者
+	//感染患者增加
 	public void addIp(String str)
 	{
 		str = str.substring(0,str.length()-1);
 		infectionPatients += Integer.parseInt(str);
 	}	
-	//治愈患者
+	//疑似患者减少
+	public void removeSp(String str)
+	{
+		str = str.substring(0,str.length()-1);
+		suspectedPatients -= Integer.parseInt(str);
+	}
+	//感染患者减少
+	public void removeIp(String str)
+	{
+		str = str.substring(0,str.length()-1);
+		infectionPatients -= Integer.parseInt(str);
+	}
+	//治愈患者增加
 	public void cure(String str)
 	{
 		str = str.substring(0,str.length()-1);
 		cure += Integer.parseInt(str);
 		infectionPatients -= Integer.parseInt(str);
 	}	
-	//患者死亡
+	//死亡患者增加
 	public void dead(String str)
 	{
 		str = str.substring(0,str.length()-1);
 		dead += Integer.parseInt(str);
 		infectionPatients -= Integer.parseInt(str);
-	}	
-	//排除疑似患者
-	public void remove(String str)
+	}		
+	//统计全国的情况
+	public void allAdd(Province p)
 	{
-		str = str.substring(0,str.length()-1);
-		suspectedPatients -= Integer.parseInt(str);
+		this.infectionPatients += p.infectionPatients;
+		this.suspectedPatients += p.suspectedPatients;
+		this.cure += p.cure;
+		this.dead += p.dead;
 	}
 	//输出本省情况
 	public void output()

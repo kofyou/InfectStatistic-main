8d8f8daeff8d94a747a92efe531008387840d354
ldc-37 <ldc791551236@gmail.com>
2020-02-11 21:29:30 +0800

    copy files & add codestyle

---
+++ 221701331/src/InfectStatistic.java
@@ -0,0 +1,12 @@
+/**
+ * InfectStatistic
+ *
+ * @author ldc
+ * @version 1.0
+ * @since xxx
+ */
+class InfectStatistic {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}

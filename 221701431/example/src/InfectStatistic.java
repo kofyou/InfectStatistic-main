e703c17deffbb4c1a8d86797fe1e80c3815afbf3
jokercan <942848439@qq.com>
2020-02-11 21:51:50 +0800

    judge -log

--- 221701431/example/src/InfectStatistic.java
+++ 221701431/example/src/InfectStatistic.java
@@ -16,19 +16,33 @@ import java.util.regex.Pattern;
 class InfectStatistic {
     public static void main(String[] args) {
         int a = new Execute().judge(args);
-//        new Test().test(args);
+//        new Test().test();
 //        new Execute().log(args);
     }
 }
 
 class Test{
-    public void test(String[] a){
-        String pro = "福建|全国";
-        for(int i = 0 ; i < a.length ; i++){
-            if (Pattern.matches(pro , a[i])){
-                System.out.println(a[i]);
-            }
+    public void test(){
+//        String pro = "福建|全国";
+        String dateFile = "((([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-" +
+                "(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|" +
+                "(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|" +
+                "((0[48]|[2468][048]|[3579][26])00))-02-29)(([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|" +
+                "[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|" +
+                "((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))|" +
+                "((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29))\\.log\\.txt";
+//        String dateFile2 = "";
+        File file = new File("/Users/a/Desktop/近期作业/InfectStatistic-main/221701431/example/log");
+        File[] files = file.listFiles();
+        for (int i = 0 ; i < files.length ; i++){
+                System.out.println(files[i].getName());
         }
+        for (int i = 0 ; i < files.length ; i++){
+            if (Pattern.matches(dateFile , files[i].getName()))
+                System.out.println(files[i].getName());
+        }
+
+        System.out.println(dateFile);
     }
 
 }
@@ -52,13 +66,13 @@ class Execute{
           "((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29)";
   static String province = "全国|北京|天津|上海|重庆|河北|山西|辽宁|吉林|黑龙江|江苏|浙江|安徽|福建|江西|山东|" +
           "河南|湖北|湖南|广东|海南|四川|贵林|云南|陕西|甘肃|青海|台湾|内蒙古|广西|西藏|宁夏|新疆|香港|澳门";
-  static String logFile = "(([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-" +
+  static String logFile = "((([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-" +
           "(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|" +
           "(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|" +
           "((0[48]|[2468][048]|[3579][26])00))-02-29)(([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|" +
           "[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|" +
           "((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))|" +
-          "((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29).log.txt";
+          "((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29))\\.log\\.txt";
 
   public void log(String[] args){
 
@@ -89,21 +103,25 @@ class Execute{
                       integers.add(i);
                   }
               }
+//              不允许不存在参数的情况
               if (integers.size() == 0){
                   System.out.println("请输入相应的参数及参数的选项");
                   return 0;
               }
+
+//              list之后第一个必须是语法的参数
               if (integers.get(0) != 1){
-                  System.out.println("Error Command");
+                  System.out.println("Error Command1");
                   return 0;
               }
 
+//              必须存在-out 和 -log 两个参数
               int ExistMust = 0;
               for (int i = 0 ; i < integers.size() ; i++){
                   if (args[integers.get(i)].equals("-log")){
                       ExistMust++;
                   }
-                  if (args[integers.get(i)].equals("-log")){
+                  if (args[integers.get(i)].equals("-out")){
                       ExistMust++;
                   }
               }
@@ -112,30 +130,81 @@ class Execute{
                   return 0;
               }
 
+              for (int i = 0 ; i < integers.size() ; i++){
+//                  两个参数之间必须有间隔
+                  if (i != integers.size()-1){
+                      if ((integers.get(i+1) - integers.get(i)) < 1 ){
+                          System.out.println("Error Command2");
+                          return 0;
+                      }
+                  }
+                  else {
+                      if (integers.get(i) >= args.length -1){
+                          System.out.println("Error Command3");
+                          return 0;
+                      }
+                  }
+              }
 
-
+//              参数需要特定的这几种形式
+              for (int i = 0 ; i < integers.size() ; i++){
+                  if (!args[integers.get(i)].equals("-log") && !args[integers.get(i)].equals("-out") &&
+                          !args[integers.get(i)].equals("-type") && !args[integers.get(i)].equals("-date") &&
+                          !args[integers.get(i)].equals("-province")){
+                      System.out.println("Error Command4");
+                      return 0;
+                  }
+              }
 
               for (int i = 0 ; i < integers.size() ; i++){
-
+                  /*
+                  * -log 后面只能跟一个参数
+                  *     参数必须是一个目录文件
+                  *     目录文件下面必须存在有形如YYYY-mm-dd.log.txt的文件
+                  * */
                   if (args[integers.get(i)].equals("-log")){
-
                       String logInputPath;
                       if ((i+1) == integers.size()){
                           File file = new File(args[integers.get(i)+1]);
                           if (!file.isDirectory()){
-                              System.out.println("The Directory is empty");
+                              System.out.println("It should be a directory");
                           }
                           else {
                               File[] fileList = file.listFiles();
+                              int judge = 0;
                               for (int j = 0 ; j < fileList.length ; j++){
                                   if (Pattern.matches(logFile , fileList[j].getName())){
-                                      break;
+                                      judge++;
                                   }
+                              }
+                              if (judge == 0){
+                                  System.out.println("File of log not found");
+                                  return 0;
                               }
                           }
                       }
                       else {
-
+                          if ((integers.get(i+1) - integers.get(i)) != 2){
+                              System.out.println("Error Command5");
+                              return 0;
+                          }
+                          File f = new File(args[integers.get(i)+1]);
+                          if (!f.isDirectory()){
+                              System.out.println("It should be a directory");
+                          }
+                          else {
+                              File[] files = f.listFiles();
+                              int judge = 0;
+                              for (int j = 0 ; j < files.length ; j++){
+                                  if (Pattern.matches(logFile , files[j].getName())){
+                                      judge++;
+                                  }
+                              }
+                              if (judge == 0){
+                                  System.out.println("File of log not found");
+                                  return 0;
+                              }
+                          }
                       }
                   }
 
@@ -155,13 +224,15 @@ class Execute{
                   if (args[integers.get(i)].equals("-province")){
 
                   }
+
+
               }
 
           }
 
           if (args[0].equals("help")){
               if (args.length > 1){
-                  System.out.println("Comman error");
+                  System.out.println("Command help don't need parameter");
                   return 0;
               }
               return 2;
@@ -199,11 +270,7 @@ class List{
 * 增加的功能，显示命令用法
 * */
 class Help{
-    public void helpList(){
-
-    }
-
-    public void help(String command){
+    public void help(){
 
     }
 

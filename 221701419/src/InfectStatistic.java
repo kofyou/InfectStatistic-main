5bb56c4c9964c23d8e74781f426bdbfb35ec81b2
zoro <904566722@qq.com>
2020-02-12 18:58:35 +0800

    创建Province类，完成codestyle、.gitignore

--- 221701419/src/InfectStatistic.java
+++ 221701419/src/InfectStatistic.java
@@ -1,13 +1,46 @@
-/**
+
+ /**
  * InfectStatistic
- * TODO
- *
- * @author xxx
- * @version xxx
- * @since xxx
+ * @author HHQ
+ * @version 1.0
  */
 class InfectStatistic {
+
+    /**
+     * Province类
+     * @author HHQ
+     */
+    public class Province {
+
+        /** 省份名称 */
+        String provinceName; 
+        /** 感染患者 */
+        int ip; 
+        /** 疑似患者 */
+        int sp;
+        /** 治愈 */
+        int cure;
+        /** 死亡 */
+        int dead;
+
+        Province(String provinceName, int ip, int sp, int cure, int dead) {
+            this.provinceName = provinceName;
+            this.ip = ip;
+            this.sp = sp;
+            this.cure = cure;
+            this.dead = dead;
+        }
+
+    }
+
+    /**
+     * description：静态的一个工具类，一些静态的方法
+     * @author HHQ
+     */
+    static class ToolMethods {
+    
+    }
+
     public static void main(String[] args) {
-        System.out.println("helloworld");
     }
 }

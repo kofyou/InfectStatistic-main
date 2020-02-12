1fdcbe18473e10233c073d970071185d0d7d61b0
SpringAlex <49441298+SpringAlex@users.noreply.github.com>
2020-02-12 23:25:03 +0800

    新增命令行参数检测
    命令行参数检测，检测和识别参数类型。

--- 221701131/example/src/InfectStatistic.java
+++ 221701131/example/src/InfectStatistic.java
@@ -8,6 +8,142 @@
  */
 class InfectStatistic {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        parameterOption parOpt=new parameterOption(args);
+		parOpt.process();
     }
 }
+
+class parameterOption{
+	private String[] myArgs;
+	private Lib lib;
+	private String log;
+	private String out;
+	private String date=null;
+	private int[] province=null;
+	private int[] type= null;
+	
+	
+	public parameterOption(String []args) {
+		//构造函数
+		myArgs=args;
+		//持有lib 对象
+		lib=new Lib();
+		
+	}
+	
+	public void process() {
+		if(myArgs.length==0) {
+			//System.out.println("没有参数");
+		}
+		else {
+			if(!myArgs[0].equals("list")) {
+				System.out.println("不以list命令开头");
+			}//if-end
+			
+			for(int i=1;i<myArgs.length;i++) {
+				if(myArgs[i].startsWith("-")) {
+					//System.out.println("包含命令段");
+					if(myArgs[i].equals(lib.commands[0])) {
+						if(i+1>=myArgs.length||myArgs[i+1].startsWith("-")) {//扫描之后的命令行(检查命令行数目)
+							System.out.println("命令不符合规范");
+							break;
+						}
+						else {
+							//把后一条命令行当作日志文件路径
+							log=myArgs[i+1];
+						}	
+					}//command-0-log
+				  if(myArgs[i].equals(lib.commands[1])) {
+					  if(i+1>=myArgs.length||myArgs[i+1].startsWith("-")) {//扫描之后的命令行(检查命令行数目)
+							System.out.println("命令不符合规范");
+							break;
+						}
+						else {
+							//把后一条命令行当作日志文件路径
+							out=myArgs[i+1];
+						}	
+				  }//command-1-out
+				  if(myArgs[i].equals(lib.commands[2])){
+					  if(i+1>=myArgs.length||myArgs[i+1].startsWith("-")) {//扫描之后的命令行(检查命令行数目)
+							//可能是省略日期，则日期为最新的一天
+						  date=null;
+						}
+					  else {
+						  date=myArgs[i+1];
+					  }
+				  }//command-2-date
+				  if(myArgs[i].equals(lib.commands[3])){
+					  boolean flag=false;
+					  type=new int[lib.types.length];
+					  if(i+1>=myArgs.length||myArgs[i+1].startsWith("-")) {//扫描之后的命令行(检查命令行数目)
+							//可能是省略，则type为所有
+						  for(int t=0;t<type.length;t++) {
+							  type[t]=1;
+						  }
+						}//if-end
+					  else {
+						  for(i++;i<myArgs.length;i++) {
+							  if(myArgs[i].startsWith("-")) {
+									flag=true;
+									break;
+								}
+								for(int j=0;j<lib.types.length;j++) {
+									//System.out.println("进入循环");
+									if(myArgs[i].equals(lib.types[j])) {
+										type[j]=1;
+									}
+								}//for-end		
+							}//for-end
+						  if(flag==true){
+							  i--;
+							  continue;
+						  }
+						  else {
+							  break;
+						  }
+					  }//else-end
+				  }//command-3-type的计数器
+				  
+				  if(myArgs[i].equals(lib.commands[4])){
+					  boolean flag2=false;
+					  province=new int[lib.provinces.length];
+					  if(i+1>=myArgs.length||myArgs[i+1].startsWith("-")) {//扫描之后的命令行(检查命令行数目)
+							//可能是省略，则province为所有
+							  province[0]=1;
+						}//if-end
+					  else {
+							for(i++;i<myArgs.length;i++) {
+								for(int j=0;j<lib.provinces.length;j++) {
+									if(myArgs[i].equals(lib.provinces[j])) {
+										province[j]=1;
+									}
+								}//for-end	
+								if(myArgs[i].startsWith("-")) {
+									flag2=true;
+									break;
+								}
+								
+							}//for-end
+							if(flag2==true){
+								  i--;
+								  continue;
+							  }
+							else {
+								break;
+							}
+					  }//else-end
+				  }//command-4-province
+				}//start with "-"
+			}//for loop -
+			if(date==null) date="最新的一天";
+			if(type==null) type= new int[]{1,1,1,1};
+			if(province==null) {
+				province=new int[] {1};
+			}
+			
+		}
+	}
+	
+}
+	
+	
--- 221701131/example/src/Lib.java
+++ 221701131/example/src/Lib.java
@@ -7,4 +7,16 @@
  * @since xxx
  */
 public class Lib {
+    public  String[] commands= {"-log","-out","-date","-type","-province"};
+	public  String[] types= {"ip","sp","cure","dead"};
+	public  String[] provinces= {
+			"全国",
+			"北京","天津","河北", "山西", "内蒙古",
+			"辽宁", "吉林", "黑龙江",
+			"上海", "江苏", "浙江", "安徽", "福建", "江西", "山东",
+			"河南", "湖北", "湖南", "广东", "广西", "海南",
+			"重庆", "四川", "贵州", "云南", "西藏",
+			"陕西", "甘肃", "青海", "宁夏", "新疆",
+			"香港", "澳门", "台湾"	
+	};
 }

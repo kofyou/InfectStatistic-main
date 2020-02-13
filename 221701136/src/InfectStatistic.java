aeb07dc5bf195b182437371bb9b5b5c9ff4938b0
tangxiaoxiong <434672608@qq.com>
2020-02-13 20:15:53 +0800

    Revert "Revert "InfectStatistic_commit1""
    This reverts commit bc302572586a1214cc0a829b20a5e756d9d2818e.

---
+++ 221701136/src/InfectStatistic.java
@@ -0,0 +1,62 @@
+/**
+ * InfectStatistic
+ * TODO
+ *
+ * @author 唐小熊
+ * @version 1.0
+ * @since 2.13
+ * @function 统计疫情数据
+ */
+
+import java.io.*;
+
+import com.sun.org.apache.xpath.internal.operations.String;
+
+class InfectStatistic {
+	
+	
+	
+	
+	/*
+	 *函数功能：解析命令行
+	 *输入参数：命令行字符串
+	 *输出参数：-data、-type、-province、-log、-out
+	 **/
+	public void analyseCommandLine(String args[])throws IOException {
+	
+	}
+	
+	/*
+	 *函数功能：读取日志
+	 *输入参数：-log路径
+	 *输出参数：省份新增感染人数、疑似人数、治愈人数、死亡人数、流入人数、流出人数
+	 **/
+	public void readData(string path) {
+		File f=new File()
+	}
+	
+	/*
+	 *函数功能：统计省份疫情人数
+	 *输入参数：
+	 *输出参数：
+	 **/
+	public void countData() {
+		
+		
+	}
+	
+	/*
+	 *函数功能：输出统计结果到文件中
+	 *输入参数：
+	 *输出参数：
+	 **/
+	public void outputData() {
+		
+		
+	}
+	
+	
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}

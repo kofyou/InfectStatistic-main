4b3fdc190cc935b4f737f4934220e6bcdba07a51
esin <550826264@qq.com>
2020-02-14 17:49:57 +0800

    增加gitignore以及codestyle，写了部分代码

--- 221701224/src/InfectStatistic.java
+++ 221701224/src/InfectStatistic.java
@@ -6,8 +6,54 @@
  * @version xxx
  * @since xxx
  */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.*;
+
 class InfectStatistic {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        /*if(args[0].equals("list"))
+        {
+        	for(int i=1;i<args.length;i++)
+        	{
+        		if(args[i].equals("-log"))
+        		{
+        			File file=new File(args[i+1]);
+        		}
+        		if(args[i].equals("-out"))
+        		{
+        			
+        		}
+        	}
+        }*/
+    	
+    	
     }
+
+
+public static List<String> getFiles(String path) {
+    List<String> files = new ArrayList<String>();
+    File file = new File(path);
+    File[] tempList = file.listFiles();
+
+    for (int i = 0; i < tempList.length; i++) {
+        if (tempList[i].isFile()) {
+            files.add(tempList[i].toString());
+            //文件名，不包含路径
+            //String fileName = tempList[i].getName();
+        }
+        if (tempList[i].isDirectory()) {
+            //这里就不递归了，
+        }
+    }
+    return files;
+}
+
+
+
 }

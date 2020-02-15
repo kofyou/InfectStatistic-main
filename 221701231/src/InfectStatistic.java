d5d55d3e450dba5c652f9e7bc634a6cbf8ee0e05
RadishBEAR <44549468+RadishBEAR@users.noreply.github.com>
2020-02-15 19:17:18 +0800

    复制example下的目录结构

---
+++ 221701231/src/InfectStatistic.java
@@ -0,0 +1,13 @@
+/**
+ * InfectStatistic
+ * TODO
+ *
+ * @author xxx
+ * @version xxx
+ * @since xxx
+ */
+class InfectStatistic {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}

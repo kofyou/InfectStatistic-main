56f3b54336bb3f9358a6841496037947fccc1c32
ququdefuqin <1665605828@qq.com>
2020-02-15 17:02:55 +0800

    命令行参数处理
    实现命令行参数处理,将命令行参数及其对应值存到map里面

--- 221701239/src/InfectStatistic.cpp
+++ 221701239/src/InfectStatistic.cpp
@@ -7,11 +7,50 @@
 #include<iostream>
 #include<fstream>
 #include<filesystem>
-#include <string>  
+#include <string>
+#include<string.h>
 #include <io.h>  
 #include <vector>  
+#include<map>
 
 using namespace std;
+map<string, string> m_single;      //存储命令行只有一个值的元素的信息
+map<string, vector<string>> m_many;   //存储命令行可能有多个值的元素的信息
+
+
+									  //该函数实现了对命令行信息的处理
+void process_cmd(int num, char* cmd_i[])
+{
+	int i;
+	string temp;   //中转字符串
+	for (i = 3; i < num; i++)
+	{
+		if (strcmp(cmd_i[i], "-province")==0 || strcmp(cmd_i[i], "-type")==0)
+		{
+			vector<string> list_temp;
+			string m_name = cmd_i[i];//记录命令行参数名称
+			temp = cmd_i[i];
+			while (temp.compare("-") != 0)
+			{
+				temp = cmd_i[++i];
+				list_temp.push_back(temp);
+				cout << cmd_i[i] << "\n";
+				if (i >= num - 1)
+					break;
+				temp=temp.substr(0, 1);
+			}
+			m_many.insert(make_pair(m_name, list_temp));
+		}
+		else
+		{
+			string temp1, temp2;
+			temp1 = cmd_i[i];
+			temp2 = cmd_i[++i];
+			m_single.insert(make_pair(temp1, temp2));
+		}
+	}
+}
+
 //该函数实现了将path路径文件夹下的所有文件读取到files里面
 void getFiles(const std::string & path, std::vector<std::string> & files)
 {
@@ -43,6 +82,7 @@ void getFiles(const std::string & path, std::vector<std::string> & files)
 
 int main(int argc, char* argv[])
 {
+	process_cmd(argc, argv);
 	int i;
 	cout << argc;
 	for (i = 0; i < argc; i++)
@@ -53,7 +93,7 @@ int main(int argc, char* argv[])
 	getFiles("D:\\肺炎代码\\InfectStatistic-main\\221701239\\log", file_list);
 	for (i = 0; i < file_list.size(); i++)
 	{
-		cout << file_list[i] << "\n";
+		//cout << file_list[i] << "\n" ;
 	}
 	system("pause");
 

e876e12094c4b5d183234d65d2280ea012c66e2a
ququdefuqin <1665605828@qq.com>
2020-02-15 11:07:26 +0800

    读取文件
    实现了读取文件夹下所有文件的功能

--- 221701239/src/InfectStatistic.cpp
+++ 221701239/src/InfectStatistic.cpp
@@ -12,14 +12,49 @@
 #include <vector>  
 
 using namespace std;
+//该函数实现了将path路径文件夹下的所有文件读取到files里面
+void getFiles(const std::string & path, std::vector<std::string> & files)
+{
+	//文件句柄  
+	long long hFile = 0;
+	//文件信息，_finddata_t需要io.h头文件  
+	struct _finddata_t fileinfo;
+	std::string p;
+	int i = 0;
+	if ((hFile = _findfirst(p.assign(path).append("\\*").c_str(), &fileinfo)) != -1)
+	{
+		do
+		{
+			//如果是目录,迭代之  
+			//如果不是,加入列表  
+			if ((fileinfo.attrib & _A_SUBDIR))
+			{
+				//if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0)
+				//getFiles(p.assign(path).append("\\").append(fileinfo.name), files);
+			}
+			else
+			{
+				files.push_back(p.assign(path).append("\\").append(fileinfo.name));
+			}
+		} while (_findnext(hFile, &fileinfo) == 0);
+		_findclose(hFile);
+	}
+}
 
 int main(int argc, char* argv[])
 {
+	int i;
 	cout << argc;
-	for (int i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		cout << argv[i] << "\n";      //命令行测试
 	}
+	vector<string> file_list;
+	getFiles("D:\\肺炎代码\\InfectStatistic-main\\221701239\\log", file_list);
+	for (i = 0; i < file_list.size(); i++)
+	{
+		cout << file_list[i] << "\n";
+	}
 	system("pause");
-	
+
 }

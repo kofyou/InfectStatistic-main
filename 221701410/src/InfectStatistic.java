d4c68fd00d7ac49184a7668765b1998d4b24c464
hujh4779 <823362797@qq.com>
2020-02-15 16:10:21 +0800

    Create DataHandle
    增加数据处理类，写了数据处理的方法

--- 221701410/src/InfectStatistic.java
+++ 221701410/src/InfectStatistic.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 /**
  * InfectStatistic
@@ -24,6 +25,8 @@ class InfectStatistic {
         if (!logHandle.readLogs(commandHandle.getEndDate(), commandHandle.getLogPath())) {
             System.out.println("日期超出范围");
         }
+        DataHandle dataHandle = infectStatistic.new DataHandle();
+        dataHandle.dataProcess(logHandle.getStringList());
     }
 
     class CommandHandle {
@@ -164,6 +167,52 @@ class InfectStatistic {
                 }
             }
             return true;
+        }
+    }
+
+    class DataHandle {
+        private String[] provinceString = {"全国", "安徽", "澳门" ,"北京", "重庆", "福建","甘肃","广东", "广西", "贵州",
+                "海南", "河北", "河南", "黑龙江", "湖北", "湖南", "吉林","江苏", "江西", "辽宁", "内蒙古", "宁夏", "青海",
+                "山东", "山西", "陕西", "上海","四川", "台湾", "天津", "西藏", "香港", "新疆", "云南", "浙江"};
+        private int[][] patient = new int[32][4];
+
+        public void dataProcess(ArrayList<String> stringList) {
+            String pattern1 = "\\W+ 新增 感染患者 \\d+人";
+            String pattern2 = "\\W+ 新增 疑似患者 \\d+人";
+            String pattern3 = "\\W+ 感染患者 流入 \\W+ \\d+人";
+            String pattern4 = "\\W+ 疑似患者 流入 \\W+ \\d+人";
+            String pattern5 = "\\W+ 死亡 \\d+人";
+            String pattern6 = "\\W+ 治愈 \\d+人";
+            String pattern7 = "\\W+ 疑似患者 确诊感染 \\d+人";
+            String pattern8 = "\\W+ 排除 疑似患者 \\d+人";
+            for(int i = 0; i < stringList.size(); i++){
+                String str = stringList.get(i);
+                if (Pattern.matches(pattern1, str)) {
+                    ipAdd(str);
+                }
+                else if (Pattern.matches(pattern2, str)) {
+                    spAdd(str);
+                }
+                else if (Pattern.matches(pattern3, str)) {
+                    ipFlow(str);
+                }
+                else if (Pattern.matches(pattern4, str)) {
+                    spFlow(str);
+                }
+                else if (Pattern.matches(pattern5, str)) {
+                    deadAdd(str);
+                }
+                else if (Pattern.matches(pattern6, str)) {
+                    cureAdd(str);
+                }
+                else if (Pattern.matches(pattern7, str)) {
+                    spToIp(str);
+                }
+                else if (Pattern.matches(pattern8, str)) {
+                    spSub(str);
+                }
+            }
+
         }
     }
 

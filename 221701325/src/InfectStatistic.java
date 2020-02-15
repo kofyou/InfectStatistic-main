a8746f60cd7f863a52a308d49159c5d8c9a6dbe7
gnep_glb <benjamin_glb@foxmail.com>
2020-02-15 12:29:01 +0800

    chore(Lib): add the TxtTool to manage the .txt file

--- 221701325/src/InfectStatistic.java
+++ 221701325/src/InfectStatistic.java
@@ -1,12 +1,13 @@
+import java.io.File;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 /**
  * InfectStatistic
- * 使用Map对list命令的参数进行储存并处理
+ * 利用CmdArgs类对命令行进行剖析
+ * 利用ListCommand类和Command类的命令模式保证程序可拓展性
+ * 使用Map对list命令的参数和值进行储存并处理
  *
  * @author Benjamin_Gnep
  * @version 1.0
@@ -16,111 +17,26 @@ class InfectStatistic {
     public static void main(String[] args) {
     	String[] test = {"list","-date","2020-01-22","2020-02-14","-log","D:/log/","-out","D:/output.txt"};
     	CmdArgs cmdArgs = new CmdArgs(test,"benjamin");
-    	ListCommand listCommand = new ListCommand();//目前默认只有list命令
     	Map<String,List<String>> map = new HashMap<String,List<String>>();
     	
-    	
+    	/*
+    	 * 这里可能省略一些命令的选择，可以添加list之外的命令进行筛选
+    	 */
+
+    	ListCommand listCommand = new ListCommand();//目前默认只有list命令
     	cmdArgs.setCommand(listCommand);
     	cmdArgs.fillMap(map);
     	cmdArgs.command.execute(map);
     	
     	System.out.println(map);
+    	
+    	TxtTool txtTool = new TxtTool();
+    	
+    	File file = new File("C:\\Users\\jhuy\\Documents\\GitHub\\InfectStatistic-main\\example\\log\\2020-01-22.log.txt");
+    	String test1 = TxtTool.txt2String(file);
+    	System.out.println(test1);
+    	File file2 = new File("C:\\Users\\jhuy\\Documents\\GitHub\\InfectStatistic-main\\example\\log\\output.txt");
+    	TxtTool.string2Txt(file2);
     }
     
 }
-
-
-
-
-/**
- *存储命令行参数
- *将命令行通过键值对的方式储存至map中
- */
-class CmdArgs {
-    String[] args;
-    Command command;
-    int index = 1;//index 作为现在处理到的位置,跳过前面命令list,否则为0
-    
-    /**
-     * 传入命令行参数数组构造
-     * @param args
-     */
-    CmdArgs(String[] args) {
-        this.args = args;
-    }
-
-	/**
-     * 传入命令构造，可以设置无用的前缀，如`groovy Lib`
-     * @param argsStr
-     * @param noUseInStart
-     */
-    CmdArgs(String[] argsStr, String noUseInStart) {
-    	this.args = argsStr;
-    }
-    
-    /**
-     * 确认命令
-     * @param command
-     */
-    public void setCommand(Command command) {
-        this.command = command;
-    }
-
-    /**
-     * 填满map并返回
-     * @param map
-     * @return	map
-     */
-    public Map<String, List<String>> fillMap(Map<String, List<String>> map) {
-		String key;
-		List<String> value;
-		while(index < args.length) {
-			key = argKey();
-			key = key.toLowerCase().trim();
-			
-			value = argVals();
-//			Iterator<String> it = value.iterator();
-//	    	while(it.hasNext()) {
-//	    		System.out.println(it.next());
-//	    	}
-			map.put(key, value);
-		}
-		return map;
-	}
-    
-
-	/**
-     * 获取命令中的一个参数
-     * @return
-     */
-    String argKey() {
-    	if(index<args.length && args[index].matches("^\\-.*$")) {//index 作为现在处理到的位置
-    		String[] key = args[index].split("\\-");
-    		index++;
-    		//System.out.println(key[1]);
-    		return key[1];
-    	}
-		return null;
-    }
-
-    /**
-     * 获取某个命令行参数的值，返回列表
-     * 同时判断该命令是否有对应的参数，若无参数则设为default
-     * @param key
-     * @return
-     */
-	List<String> argVals() {
-    	List<String> values = new LinkedList<String>();
-    	while(index<args.length && (!args[index].matches("^\\-.*$"))) {//将参数所有的值放入values中
-    		values.add(args[index]);
-    		index++;
-    	}
-		Iterator<String> it = values.iterator();
-    	if(!it.hasNext()) {
-    		values.add("default");
-    	}
-    	return values;
-    }
-}
-
-
--- 221701325/src/Lib.java
+++ 221701325/src/Lib.java
@@ -1,13 +1,21 @@
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.regex.Pattern;
 
 /**
  * Lib
  * 1.利用责任链模式对日志文件进行处理
- * 
+ * 2.ListCommand类的实现
+ * 3.CmdArgs类的实现
+ * 4.
  *
  * @author Benjamin_Gnep
  * @version 1.0
@@ -15,6 +23,98 @@ import java.util.regex.Pattern;
  */
 public class Lib {
 }
+
+
+/**
+ *存储命令行参数
+ *将命令行通过键值对的方式储存至map中
+ */
+class CmdArgs {
+    String[] args;
+    Command command;
+    int index = 1;//index 作为现在处理到的位置,跳过前面命令list,否则为0
+    
+    /**
+     * 传入命令行参数数组构造
+     * @param args
+     */
+    CmdArgs(String[] args) {
+        this.args = args;
+    }
+
+	/**
+     * 传入命令构造，可以设置无用的前缀，如`groovy Lib`
+     * @param argsStr
+     * @param noUseInStart
+     */
+    CmdArgs(String[] argsStr, String noUseInStart) {
+    	this.args = argsStr;
+    }
+    
+    /**
+     * 确认命令
+     * @param command
+     */
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    /**
+     * 填满map并返回
+     * @param map
+     * @return	map
+     */
+    public Map<String, List<String>> fillMap(Map<String, List<String>> map) {
+		String key;
+		List<String> value;
+		while(index < args.length) {
+			key = argKey();
+			key = key.toLowerCase().trim();
+			
+			value = argVals();
+//			Iterator<String> it = value.iterator();
+//	    	while(it.hasNext()) {
+//	    		System.out.println(it.next());
+//	    	}
+			map.put(key, value);
+		}
+		return map;
+	}
+
+	/**
+     * 获取命令中的一个参数
+     * @return
+     */
+    String argKey() {
+    	if(index<args.length && args[index].matches("^\\-.*$")) {//index 作为现在处理到的位置
+    		String[] key = args[index].split("\\-");
+    		index++;
+    		//System.out.println(key[1]);
+    		return key[1];
+    	}
+		return null;
+    }
+
+    /**
+     * 获取某个命令行参数的值，返回列表
+     * 同时判断该命令是否有对应的参数，若无参数则设为default
+     * @param key
+     * @return
+     */
+	List<String> argVals() {
+    	List<String> values = new LinkedList<String>();
+    	while(index<args.length && (!args[index].matches("^\\-.*$"))) {//将参数所有的值放入values中
+    		values.add(args[index]);
+    		index++;
+    	}
+		Iterator<String> it = values.iterator();
+    	if(!it.hasNext()) {
+    		values.add("default");
+    	}
+    	return values;
+    }
+}
+
 
 
 enum ListKey{
@@ -34,13 +134,13 @@ class ListCommand implements Command{
 	
 	@Override
 	public void execute(Map<String, List<String>> map) {
+		
 		Set<String> keySet = map.keySet();
 		Iterator<String> it =keySet.iterator();
+		
 		while(it.hasNext()) {
 			String key = it.next();
 			List<String> value = map.get(key);
-			
-			
 			switch (key) {
 				case "date":
 					listKey = ListKey.DATE;
@@ -106,6 +206,43 @@ class ListCommand implements Command{
 
 }
 
+class TxtTool {
+    public static String txt2String(File file){
+        StringBuilder result = new StringBuilder();
+        
+        try{
+            String s = null;
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            while((s = br.readLine())!=null){//使用readLine方法，一次读一行
+            	//若本行为'//'开头说明为注释，应忽略
+            	if(s.matches("^\\/\\/.*$"))
+            		continue;
+                result.append(System.lineSeparator()+s);
+            }
+            br.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        return result.toString();
+    }
+    
+    public static void string2Txt(File file) {
+        try {
+            file.createNewFile(); // 创建新文件,有同名的文件的话直接覆盖
+            FileWriter writer = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(writer);
+            bw.write("我会写入文件啦1\r\n"); // \r\n即为换行
+            bw.write("我会写入文件啦2\r\n"); // \r\n即为换行
+            bw.flush(); // 把缓存区内容压入文件
+            bw.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
 
 //责任链模式处理日志文件
 abstract class MyHandler{

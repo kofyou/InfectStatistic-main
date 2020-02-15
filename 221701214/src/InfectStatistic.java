9649935af1456add12f1c80cb506f741dbf0c201
SIPUNK <50772781+SIPUNK@users.noreply.github.com>
2020-02-15 22:47:47 +0800

    修改日志文件处理的相应bug
    目前输出文件仍有bug

--- 221701214/src/InfectStatistic.java
+++ 221701214/src/InfectStatistic.java
@@ -1,4 +1,5 @@
-package hw2;
+
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,12 +12,11 @@ public class Homework2
 {
 	
 	public String[] allType = {"感染患者","疑似患者","治愈","死亡患者"};
-	public String[] allProvinces = {"安徽","北京","重庆","福建","甘肃","广东","广西","贵州","海南","河北"
+	public String[] allProvinces = {"全国","安徽","北京","重庆","福建","甘肃","广东","广西","贵州","海南","河北"
 									,"河南","黑龙江","湖北","湖南","吉林","江苏","江西","辽宁","内蒙古","宁夏"
 									,"青海","山东","山西","陕西","上海","四川","天津","西藏","新疆","云南","浙江"};
-	public int[][] people = new int[31][4];//三十一个省份，四种类型
+	public int[][] people = new int[32][4];//三十一个省份（加全国），四种类型
 	ArrayList<String> allLog = new ArrayList<String>();//存储所有的日志文件
-	
 	
 	
 	/*
@@ -137,30 +137,31 @@ public class Homework2
 				break;
 			}
 		}
+		//System.out.print(patternNum);
 		switch(patternNum)
 		{
-			case '0' :
+			case 0 :
 				addIp(inputLine);//新增感染患者
 				break;
-			case '1' :
+			case 1 :
 				addSp(inputLine);//新增疑似患者
 				break;
-			case '2' :
+			case 2 :
 				inflowIp(inputLine);//确诊患者流入
 				break;
-			case '3' :
+			case 3 :
 				inflowSp(inputLine);//疑似患者流入
 				break;
-			case '4' :
+			case 4 :
 				deadIp(inputLine);//患者死亡
 				break;
-			case '5' :
+			case 5 :
 				cureIp(inputLine);//患者治愈
 				break;
-			case '6' :
+			case 6 :
 				diagnoseSp(inputLine);//疑似患者确诊
 				break;
-			case '7' :
+			case 7 :
 				removeSp(inputLine);//排除疑似患者
 				break;
 			default :
@@ -188,6 +189,7 @@ public class Homework2
 			if(allProvinces[j].equals(words[0]))
 			{
 				people[j][0] += ipNum;
+				people[0][0] += ipNum;
 			}
 		}
 	}
@@ -212,6 +214,7 @@ public class Homework2
 			if(allProvinces[j].equals(words[0]))
 			{
 				people[j][1] += spNum;
+				people[0][1] += spNum;
 			}
 		}
 	}
@@ -290,6 +293,9 @@ public class Homework2
 			if(allProvinces[j].equals(words[0]))
 			{
 				people[j][2] += cureNum;
+				people[0][2] += cureNum;
+				people[j][0] -= cureNum;
+				people[0][0] -= cureNum;
 			}
 		}
 	}
@@ -313,6 +319,9 @@ public class Homework2
 			if(allProvinces[j].equals(words[0]))
 			{
 				people[j][3] += deadNum;
+				people[0][3] += deadNum;
+				people[j][0] -= deadNum;
+				people[0][0] -= deadNum;
 			}
 		}
 	}
@@ -330,12 +339,15 @@ public class Homework2
 				addNum += words[3].charAt(i);
 			}
 		}
-		int ipNum = Integer.valueOf(addNum).intValue();
+		int spNum = Integer.valueOf(addNum).intValue();
 		for(int j = 0;j < allProvinces.length;j++)
 		{
 			if(allProvinces[j].equals(words[0]))
 			{
-				people[j][0] += ipNum;
+				people[j][0] += spNum;
+				people[j][1] -= spNum;
+				people[0][0] += spNum;
+				people[0][1] -= spNum;
 			}
 		}
 	}
@@ -359,6 +371,7 @@ public class Homework2
 			if(allProvinces[j].equals(words[0]))
 			{
 				people[j][1] -= spNum;
+				people[0][1] -= spNum;
 			}
 		}
 	}
@@ -369,32 +382,63 @@ public class Homework2
 	 */
 	public void writeOut(String filePath) throws IOException
 	{
+		/*
+		for(int i = 0;i < allProvinces.length;i++)
+        {
+        	for(int j = 0;j < allType.length;j++)
+        	{
+        		System.out.print(people[i][j]);
+        	}
+        	System.out.println();
+        }
+        */
 		File dir = new File(filePath);
 	    // 一、检查放置文件的文件夹路径是否存在，不存在则创建
-	    if (!dir.exists()) {
+	    if(!dir.exists()) 
+	    {
 	        dir.mkdirs();//创建多级目录
 	    }
-	    String[] fileName = filePath.split("/");
+	    String[] fileName = filePath.split("\\");
 	    File checkFile = new File(filePath + fileName[fileName.length-1]);
 	    FileWriter writer = null;
-	    try {
+	    try 
+	    {
 	        // 二、检查目标文件是否存在，不存在则创建
-	        if (!checkFile.exists()) {
+	        if(!checkFile.exists()) 
+	        {
 	            checkFile.createNewFile();// 创建目标文件
 	        }
 	        // 三、向目标文件中写入内容
 	        writer = new FileWriter(checkFile, true);
-	        writer.append("your content");
+	        for(int i = 0;i < allProvinces.length;i++)
+	        {
+	        	writer.write(allProvinces[i] + " ");
+	        	for(int j = 0;j < allType.length;j++)
+	        	{
+	        		writer.write(allType[j] + people[i][j] + "人 ");
+	        	}
+	        }
 	        writer.flush();
-	    } catch (IOException e) {
+	    } 
+	    catch (IOException e) 
+	    {
 	        e.printStackTrace();
-	    } finally {
+	    } 
+	    finally 
+	    {
 	        if (null != writer)
 	            writer.close();
 	    }
 	}
-	public static void main(String args[])
+	public static void main(String args[]) throws IOException
 	{
+		if(args.length == 0)
+		{
+			System.out.println("未输入命令行参数");
+			return ;
+		}
+		Homework2 hw2 = new Homework2();
+		hw2.orderHandle(args);
 		
 	}
 }

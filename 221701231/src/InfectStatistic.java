63b0a32e3f3fffff9987c744993ed9912d8ef9ca
RadishBEAR <44549468+RadishBEAR@users.noreply.github.com>
2020-02-16 17:04:08 +0800

    第四次修改InfectStatistic.java——加入了初步的文件处理
    加入文件处理器类FileProcessor，实现了按照输出目录创建文件的功能

--- 221701231/src/InfectStatistic.java
+++ 221701231/src/InfectStatistic.java
@@ -1,10 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+
 /**
  * InfectStatistic
  * TODO
  *
  * @author 221701231_朱鸿昊
- * @version 1.0.3
- * @since 2020/2/16 15.28
+ * @version 1.0.4
+ * @since 2020/2/16 17:01
  */
 class InfectStatistic {
     static class Controller{
@@ -89,11 +92,48 @@ class InfectStatistic {
         }
     }
 
+    static class FileProcessor{
+        public static void CreateOutputFile(){
+            String path=Controller.outputLocation;
+            String reversedFileName="";
+            String fileName="";
+
+            //获取输出文件名
+            for(int i0 = Controller.outputLocation.length()-1;i0 > 0;i0--){
+                if(Controller.outputLocation.charAt(i0)=='/'){
+                    break;
+                }
+                reversedFileName += Controller.outputLocation.charAt(i0);
+            }
+            for(int i0 = reversedFileName.length()-1;i0 >= 0;i0--){
+                fileName+=reversedFileName.charAt(i0);
+            }
+
+            //获取输出文件路径
+            path=path.substring(0,path.length()-fileName.length());
+            File pathOfFile=new File(path);
+
+            if(!pathOfFile.exists())    //如果文件路径不存在则创建
+            {
+                pathOfFile.mkdirs();
+            }
+
+            //创建文件
+            BasicInformation.outputFile=new File(Controller.outputLocation);
+            try {
+                BasicInformation.outputFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     static class BasicInformation{
         public static String[] nameOfAreas = {"全国","安徽", "北京","重庆","福建","甘肃", "广东", "广西", "贵州", "海南",
                 "河北", "河南", "黑龙江", "湖北", "湖南", "吉林", "江苏", "江西", "辽宁", "内蒙古", "宁夏", "青海",
                 "山东", "山西", "陕西", "上海", "四川", "天津", "西藏", "新疆", "云南", "浙江"};
         public static AreaInformation[] Areas=new AreaInformation[32];
+        public static File outputFile;
     }
 
     static class AreaInformation{
@@ -138,9 +178,8 @@ class InfectStatistic {
             BasicInformation.Areas[i0]=new AreaInformation(BasicInformation.nameOfAreas[i0]);
         }
 
-        Controller.GetParameters(args);
-
+        Controller.GetParameters(args); // 获取输入的参数
         Controller.OutputParameters();
-
+        FileProcessor.CreateOutputFile();   // 创建输出文件
     }
 }

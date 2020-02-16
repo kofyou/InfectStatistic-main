a9d37363dea70aef928d885b50c9c852d0546789
RadishBEAR <44549468+RadishBEAR@users.noreply.github.com>
2020-02-16 19:30:33 +0800

    第五次更新InfectStatistic.java——加入了单文件读取器
    加入了单文件读取器，能够初步读取log文件信息

--- 221701231/src/InfectStatistic.java
+++ 221701231/src/InfectStatistic.java
@@ -1,13 +1,12 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * InfectStatistic
  * TODO
  *
  * @author 221701231_朱鸿昊
- * @version 1.0.4
- * @since 2020/2/16 17:01
+ * @version 1.0.5
+ * @since 2020/2/16 19:28
  */
 class InfectStatistic {
     static class Controller{
@@ -59,6 +58,7 @@ class InfectStatistic {
                         break;
                     default:
                         Controller.designatedProvince[numberOfDesignatedProvince]=parameters[i0];
+                        BasicInformation.KeyOfOutput[BasicInformation.GetIndexOfArea(parameters[i0])] = 1;
                         numberOfDesignatedProvince++;
                 }
             }
@@ -93,6 +93,7 @@ class InfectStatistic {
     }
 
     static class FileProcessor{
+        // 创建输出文件方法
         public static void CreateOutputFile(){
             String path=Controller.outputLocation;
             String reversedFileName="";
@@ -126,14 +127,99 @@ class InfectStatistic {
                 e.printStackTrace();
             }
         }
+
+        // 读取单个文件方法
+        public static void ReadASingleFile(String path){
+            File file = new File(path);
+            StringBuilder result = new StringBuilder();
+            try{
+                //构造一个BufferedReader类来读取文件
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(new FileInputStream(file),"UTF-8"));
+                String line = null;
+                while((line = reader.readLine())!=null){
+                    if (!FileProcessor.ReadASingleLine(line)){
+                        break;
+                    }
+                }
+                reader.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        // 读取单行方法
+        public static boolean ReadASingleLine(String line){
+            String words[] = line.split(" ");
+            String word = words[0];
+            String number;  // 用于记录数字
+            int indexOfArea = 0;    // 每行最开始的地区的数组索引
+
+            // 首先判断这行是不是注释内容
+            if (word.equals("//")){
+                return false;
+            }
+
+            indexOfArea = BasicInformation.GetIndexOfArea(word);    // 获得地区索引
+
+            BasicInformation.KeyOfOutput[indexOfArea] = 1;  // 把该地区的输出key设为1
+
+            // 开始按照后面的输入操作地区类
+            for (int i0 = 1;i0 < words.length;i0++) {
+                switch (words[i0]){
+                    case ("新增"):
+                        i0++;
+                        switch (words[i0]){
+                            case ("感染患者"):
+                                i0++;
+                                number = words[i0].substring(0,words[i0].length()-1);
+                                BasicInformation.Areas[0].numberOfInfectedPatients += Integer.parseInt(number);
+                                BasicInformation.Areas[indexOfArea].numberOfInfectedPatients += Integer.parseInt(number);
+                                break;
+                            case ("疑似患者"):
+                                i0++;
+                                number = words[i0].substring(0,words[i0].length()-1);
+                                BasicInformation.Areas[0].numberOfSuspectedPatients += Integer.parseInt(number);
+                                BasicInformation.Areas[indexOfArea].numberOfSuspectedPatients += Integer.parseInt(number);
+                                break;
+                        }
+                        break;
+                    case ("治愈"):
+                        i0++;
+                        number = words[i0].substring(0,words[i0].length()-1);
+                        BasicInformation.Areas[0].numberOfPeopleCured += Integer.parseInt(number);
+                        BasicInformation.Areas[indexOfArea].numberOfPeopleCured += Integer.parseInt(number);
+                        break;
+                    case ("死亡"):
+                        i0++;
+                        number = words[i0].substring(0,words[i0].length()-1);
+                        BasicInformation.Areas[0].numberOfDeaths += Integer.parseInt(number);
+                        BasicInformation.Areas[indexOfArea].numberOfDeaths += Integer.parseInt(number);
+                        break;
+                }
+            }
+            return true;
+        }
     }
 
     static class BasicInformation{
         public static String[] nameOfAreas = {"全国","安徽", "北京","重庆","福建","甘肃", "广东", "广西", "贵州", "海南",
                 "河北", "河南", "黑龙江", "湖北", "湖南", "吉林", "江苏", "江西", "辽宁", "内蒙古", "宁夏", "青海",
                 "山东", "山西", "陕西", "上海", "四川", "天津", "西藏", "新疆", "云南", "浙江"};
-        public static AreaInformation[] Areas=new AreaInformation[32];
-        public static File outputFile;
+        public static AreaInformation[] Areas = new AreaInformation[32];
+        public static int[] KeyOfOutput = new int[32];
+        public static File outputFile;  // 输出文件
+
+        public static int GetIndexOfArea(String nameOfArea){
+            int res=0;
+            for(int i0 = 0;i0 < 32;i0++){
+                if (BasicInformation.nameOfAreas[i0].equals(nameOfArea)){
+                    res = i0;
+                    break;
+                }
+            }
+            return res;
+        }
     }
 
     static class AreaInformation{
@@ -171,15 +257,26 @@ class InfectStatistic {
             return res;
         }
     }
+
     public static void main(String[] args){
 
         // 初始化区域类
         for (int i0 = 0;i0 < BasicInformation.nameOfAreas.length;i0++){
-            BasicInformation.Areas[i0]=new AreaInformation(BasicInformation.nameOfAreas[i0]);
+            BasicInformation.Areas[i0] = new AreaInformation(BasicInformation.nameOfAreas[i0]);
+            BasicInformation.KeyOfOutput[i0] = 0;
         }
+        BasicInformation.KeyOfOutput[0] = 1;
 
         Controller.GetParameters(args); // 获取输入的参数
-        Controller.OutputParameters();
-        FileProcessor.CreateOutputFile();   // 创建输出文件
+        //FileProcessor.CreateOutputFile();   // 创建输出文件
+        System.out.println();
+        FileProcessor.ReadASingleFile(Controller.inputLocation+"2020-01-22.log.txt");
+
+        for (int i0 = 0;i0 < 32;i0++){
+            if (BasicInformation.KeyOfOutput[i0] == 1){
+                System.out.println(BasicInformation.Areas[i0].OutputInformation());
+            }
+        }
+
     }
 }

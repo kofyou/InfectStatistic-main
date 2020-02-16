b4420729e0f7b3df7419fa55237d83a3425f83a8
lqi59780808 <41891928+lqi59780808@users.noreply.github.com>
2020-02-16 19:22:41 +0800

    新增-type的部分功能
    用数组保存要显示的人群类型

--- 221701128/src/InfectStatistic.java
+++ 221701128/src/InfectStatistic.java
@@ -2,9 +2,11 @@
 public class InfectStatistic {
 	
 	public static int i;
+	public static int count;
 	public static String fileDirect;
 	public static String outputFilepath;
 	public static String dateTime;
+	public static String typePeople[];
 	
 	public static boolean judgeList (String str)
 	{
@@ -28,6 +30,11 @@ public class InfectStatistic {
 		else if(str[i].equalsIgnoreCase("-date"))
 		{
 			readDateTime(str);
+		}
+		
+		else if(str[i].equalsIgnoreCase("-type"))
+		{
+			readType(str);
 		}
 	}
 	
@@ -69,6 +76,35 @@ public class InfectStatistic {
 		System.out.print(dateTime);
 	}
 	
+	public static void readType (String str[])
+	{
+		typePeople = new String[4];
+		if(i == str.length - 1 || str[i+1].substring(0,1).equals("-"))
+		{
+			typePeople[0] = "all";
+			count++;
+		}
+		
+		else
+		{
+			i++;			
+			count = 0;
+			for(  ; i < str.length ; i++)
+			{	
+				if(str[i].equalsIgnoreCase("ip") 
+					|| str[i].equalsIgnoreCase("sp") 
+					|| str[i].equalsIgnoreCase("cure") 
+					|| str[i].equalsIgnoreCase("dead") )
+				typePeople[count++] = str[i];
+				
+				if(i == str.length - 1 || str[i+1].substring(0,1).equals("-"))  break;
+			}
+		}
+		
+		for(int k=0;k<count;k++)
+		System.out.println(typePeople[k]);
+	}
+	
 	public static void main(String args[])
     {
     	for(i = 0;i < args.length; i++)
@@ -82,3 +118,4 @@ public class InfectStatistic {
     }
 
 }
+

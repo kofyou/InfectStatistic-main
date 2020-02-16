f2a5ed0ba14349440f54ad8770cf82717483de1e
tangxiaoxiong <434672608@qq.com>
2020-02-16 23:47:08 +0800

    Update InfectStatistic.java
    一个人debug真是好难，今日份结束

--- 221701136/src/InfectStatistic.java
+++ 221701136/src/InfectStatistic.java
@@ -1,4 +1,4 @@
-/**
+/*
  * InfectStatistic
  * TODO
  *
@@ -8,62 +8,61 @@
  * @function 统计疫情数据
  */
 
-import java.io.*;
 
+import java.io.*;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.lang.*;
-import com.sun.org.apache.xpath.internal.operations.String;
+import java.lang.String;
 import java.util.*;
-import java.util.HashMap;;
-import java.util.Map;
+import java.util.HashMap;
 import java.util.regex.Pattern;
 
 
 class InfectStatistic {
 	
-	
+	private static String commandDate;
 	
 	//获取当前的系统时间并格式化输出
-	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-    Date date = new Date();
-    String currentDate = dateFormat.format(date);
+	static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    static Date date1 = new Date(System.currentTimeMillis());
+    static String currentDate = dateFormat.format(date1);
 	
 	//构造一个双层嵌套的哈希表
-	HashMap<String,HashMap<String,Integer>> ProvinceToNumMap 
-	                               = new HashMap<String,HashMap<String,Integer>>();
-	HashMap<String,Integer> TypeToNumMap = new HashMap<String,Integer>();
+	private HashMap<String,HashMap<String,Integer>> ProvinceToNumMap;
+	                              
 	
-	//初始化TypeToNum哈希表
-	TypeToNumMap.put("感染患者",0);
-    TypeToNumMap.put("疑似患者",0);
-    TypeToNumMap.put("治愈",0);
-    TypeToNumMap.put("死亡",0);
-    
-    String provinceList[] = {"全国","安徽","北京","重庆","福建","甘肃","广东","广西","贵州","海南","河北","河南",
+	
+    public InfectStatistic() {
+    	ProvinceToNumMap 
+        = new HashMap<String,HashMap<String,Integer>>();
+    	HashMap<String,Integer> TypeToNumMap = new HashMap<String,Integer>();
+    	//初始化TypeToNum哈希表
+    	TypeToNumMap.put("感染患者",0);
+        TypeToNumMap.put("疑似患者",0);
+        TypeToNumMap.put("治愈",0);
+        TypeToNumMap.put("死亡",0);
+         String provinceList[] = {"全国","安徽","北京","重庆","福建","甘肃","广东","广西","贵州","海南","河北","河南",
     		"黑龙江","湖北","湖南","吉林","江苏","江西","辽宁","内蒙古","宁夏","青海","山东","山西","陕西","上海",
-    		"四川","天津","西藏","新疆","云南","浙江"
-}
-    
-    for(int i=0;i<provinceList.length;i++) {
+    		"四川","天津","西藏","新疆","云南","浙江"};
+          for(int i=0;i<provinceList.length;i++) {
     	ProvinceToNumMap.put(provinceList[i], TypeToNumMap);
     }
-    
-    
-    String date,inputAddress,outputAddress;
-    String commandDate;
+    }
+	
+	static String inputAddress;
+	static String outputAddress;
     
 	//type和province的类型可能不止一种，故创建其字符串数组
-	List<String> typeList=new List<String>();
+	static List<String> typeList=new LinkedList<>();
 			
-	List<String> commandProvinceList=new List<String>();
+	static List<String> commandProvinceList=new LinkedList<String>();
 	
-    
 	/*
 	 *函数功能：解析命令行
 	 *输入参数：命令行字符串
 	 *输出参数：无
 	 **/
-	public static void analyseCommandLine(String args[]) {
+	public void analyseCommandLine(String args[]) {
 		String province,type;
 		
 		if(!args[0].equals("list")) {
@@ -72,7 +71,8 @@ class InfectStatistic {
 		
 		for(int commandOrder=1;commandOrder<args.length;commandOrder++) {
 				if(args[commandOrder].equals("-data")) {
-					commandDate = changeToValidDate(args[++commandOrder]);
+				//commandDate = changeToValidDate(args[++commandOrder]);
+					commandDate = args[++commandOrder];
 				}
 				if(args[commandOrder].equals("-log")) {
 					inputAddress = args[++commandOrder];
@@ -82,7 +82,7 @@ class InfectStatistic {
 					}
 				}
 				if(args[commandOrder].equals("-out")) {
-					outputAdderss = args[++commandOrder];
+					outputAddress = args[++commandOrder];
 					if(!isValidAddress(outputAddress)) {
 						System.out.println("out路径错误！");
 						return;
@@ -99,9 +99,9 @@ class InfectStatistic {
 				}
 				if(args[commandOrder].equals("-province")) {
 					province = args[commandOrder++];
-					while(!province.startwith("-")&&commandOrder<args.length-1) {
+					while(!province.startsWith("-")&&commandOrder<args.length-1) {
 						commandProvinceList.add(province);
-						province = args[commandList++];
+						province = args[commandOrder++];
 					}
 				}
 		}
@@ -113,33 +113,34 @@ class InfectStatistic {
  *输入参数：args[]
  *输出参数：true,false
  **/
-    public boolean isValidCommand(String args[]) {
+  /*  public boolean isValidCommand(String args[]) {
 	    if(!args[0].equals("list")) {
 		    System.out.println("命令行的格式有误");
 		    return false;
 	    }
     }
+    */
 
     /*
      *函数功能：获取合法的日志日期
      *输入参数：args[]
      *输出参数：Date
      **/
-    public Date changeToValidDate(String date) {
-    	SimpleDateFormat format = new SimpleDataFormat("yyyy-MM-dd");
+   /* public String changeToValidDate(String date) {
+    	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
     	Date validDate = format.parse(date);
     	return validDate;
     }
-    
+    */
     /*
      *函数功能：判断是否是合法的路径
      *输入参数：string
      *输出参数：false,true
      **/
-    public boolean isValidAddress(String address) {
+    public static boolean isValidAddress(String address) {
     	//用正则表达式判断输入的路径是否正确
-    	if(address.matches("^[A-z]:\\\\(.+?\\\\)*$")) {
-    	ruturn true;
+    	if (address.matches("^[A-z]:\\\\(.+?\\\\)*$")) {
+    	return true;
     	}
     	else
     		return false;
@@ -149,33 +150,35 @@ class InfectStatistic {
 	/*
 	 *函数功能：查询路径文件
 	 *输入参数：-log路径
-	 *输出参数：
+	 *输出参数：文件名称
 	 **/
-	public void searchFile(String inputAddress) {
+	public void searchFile(String inputAddress) throws ParseException, IOException {
 	File file = new File(inputAddress);
-	string fileName;
+	String fileName;
 	
 	//获取inputAddress路径下的所有文件和文件目录
 	File[] tempList = file.listFiles();
-
-	SimpleDateFormat format = new SimpleDataFormat("yyyy-MM-dd");
-		
-	Date commandDate = format.parse(date);
+	
+	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+	
+	Date commandDay = format.parse(commandDate);
+	Date currentDay = format.parse(currentDate);
 	
 	//若提供的日期大于当前时间，则报错
-	if(commandDate.after(currentDate)) {
-		System.out.println("日期超出范围")；
+	if(commandDay.after(currentDay)) {
+		System.out.println("日期超出范围");
 	}
 	
 	//获取所有小于commandDate的日志,并读取内容
-	for(int j = 0;j<tempList.length;j++) {
-		fileName = tempList[i].getName();
-		Date fileDate = format.parse(fileName);
-		if(fileDate.before(commandDate)) {
-			readFile(inputAddress+fileName+".log.txt");
+	for(int j=0;j<tempList.length;j++) {
+		fileName = tempList[j].getName();
+		Date fileDay = format.parse(fileName);
+		if(fileDay.before(commandDay)) {
+				readFile(inputAddress+fileName+".log.txt");
+			}
 		}	
 	}
-	}
+	
 	
 	
 	/*
@@ -184,19 +187,20 @@ class InfectStatistic {
 	 *输出参数：无
 	 **/
 	public void readFile(String address) throws IOException {
-	FileInputStream fiStream = new FileInputStream(address);
-	InputStreamReader isReader = new InputStreamReader(fiStream,"UTF-8");
-	BufferedReader bufferedReader = new BufferedReader(isReader);
-	String line = null;
-	while((line=bufferedReader.readLine())! = null) {
-		if(!line.startWith("//")) {
-			handleInformation(line);
-		}	
+		try {
+	        BufferedReader bfr = new BufferedReader(new InputStreamReader(new FileInputStream(new File(address)),"UTF-8"));
+	        String lineTxt = null;
+	                
+	        while ((lineTxt = bfr.readLine()) != null) { //按行读取文本内容
+	            if(!lineTxt.startsWith("//")) //遇到“//”不读取
+	               handleInformation(lineTxt);
+	        }
+	        bfr.close();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
 	}
-	}
-	
-	
-	
+		
 	
 	/*
 	 *函数功能：统计省份疫情人数
@@ -204,7 +208,6 @@ class InfectStatistic {
 	 *输出参数：
 	 **/
 	public void handleInformation(String lineInformation) {
-		
 		String lineTypeOne = "(\\s+) 新增 感染患者 (\\d+)人";
 		String lineTypeTwo = "(\\s+) 新增 疑似患者 (\\d+)人";
 		String lineTypeThree = "(\\s+) 治愈 (\\d+)人";
@@ -215,28 +218,28 @@ class InfectStatistic {
 		String lineTypeEight = "(\\s+) 排除 疑似患者 (\\d+)人";
 		
 		if(Pattern.matches(lineTypeOne, lineInformation)) {
-			addInfectionPatients(linePart);
+			addInfectionPatients(lineInformation);
 		}
 		if(Pattern.matches(lineTypeTwo, lineInformation)) {
-			addSuspectedPatients(linePart);
+			addSuspectedPatients(lineInformation);
 		}
 		if(Pattern.matches(lineTypeThree, lineInformation)) {
-			addCurePatients(linePart);
+			addCurePatients(lineInformation);
 		}
 		if(Pattern.matches(lineTypeFour, lineInformation)) {
-			addDeadPatients(linePart);
+			addDeadPatients(lineInformation);
 		}
 		if(Pattern.matches(lineTypeFive, lineInformation)) {
-			infectionPatientsMove(linePart);
+			infectionPatientsMove(lineInformation);
 		}
 		if(Pattern.matches(lineTypeSix, lineInformation)) {
-			suspectedPatientsMove(linePart);
+			suspectedPatientsMove(lineInformation);
 		}
 		if(Pattern.matches(lineTypeSeven, lineInformation)) {
-			suspectedToInfection(linePart);
+			suspectedToInfection(lineInformation);
 		}
 		if(Pattern.matches(lineTypeEight, lineInformation)) {
-			suspectedToNormal(linePart);
+			suspectedToNormal(lineInformation);
 		}
 		
 		
@@ -247,12 +250,13 @@ class InfectStatistic {
 	 *输入参数：
 	 *输出参数：
 	 **/
-	public int searchProvinceToTypeNum(String procince,String type) {
+	public int searchProvinceToTypeNum(String province,String type) {
 		//获取省份对应下的感染患者数量
+		int previousNum = 0;
 				Set<String> thisSet = ProvinceToNumMap.keySet();
 				for(String str:thisSet) {
 					if(str.equals(province)) {
-						HashMap<String,String> thisMap = ProvinceToNumMap.get(str);
+						HashMap<String, Integer> thisMap = ProvinceToNumMap.get(str);
 						Set<String> typeKeys = thisMap.keySet();
 						for(String strTwo:typeKeys) {
 							if(strTwo.equals(type)) {
@@ -271,7 +275,7 @@ class InfectStatistic {
 	 *输入参数：
 	 *输出参数：
 	 **/
-	public void addInfectionPatients(String linePart[]) {
+	public void addInfectionPatients(String lineInformation) {
 		
 		//先将每一行的字符串分隔成字符串数组
 		String[] linePart = lineInformation.split(" ");
@@ -285,8 +289,7 @@ class InfectStatistic {
 		
 		int previousNum,countryPreviousNum,currentNum,countryCurrentNum;
 		
-		previousNum = searchProvinceToTypeNum(province,"感染患者");
-		
+		previousNum = searchProvinceToTypeNum(province,"感染患者");	
 		countryPreviousNum = searchProvinceToTypeNum("全国","感染患者");
 			
 		currentNum = num + previousNum;
@@ -302,7 +305,7 @@ class InfectStatistic {
 	 *输入参数：
 	 *输出参数：
 	 **/
-     public void addSuspectedPatients(String linePart[]) {
+     public void addSuspectedPatients(String lineInformation) {
  		//先将每一行的字符串分隔成字符串数组
  		String[] linePart = lineInformation.split(" ");
  		String province = linePart[0];
@@ -316,7 +319,6 @@ class InfectStatistic {
  		int previousNum,countryPreviousNum,currentNum,countryCurrentNum;
  		
  		previousNum = searchProvinceToTypeNum(province,"疑似患者");
- 		
  		countryPreviousNum = searchProvinceToTypeNum("全国","疑似患者");
  			
  		currentNum = num + previousNum;
@@ -334,7 +336,7 @@ class InfectStatistic {
  	 *输入参数：
  	 *输出参数：
  	 **/
-      public void infectionPatientsMove(String linePart[]) {
+      public void infectionPatientsMove(String lineInformation) {
  		
  		//先将每一行的字符串分隔成字符串数组
  		String[] linePart = lineInformation.split(" ");
@@ -351,7 +353,7 @@ class InfectStatistic {
  		int flowOutPreviousNum,flowInPreviousNum,flowOutCurrentNum,flowInCurrentNum;
  		
  		flowOutPreviousNum = searchProvinceToTypeNum(flowOutProvince,"感染患者");
- 		flowInPreviousNum = searchProvinceToTypeNum(flowInPreviousNum,"感染患者");
+ 		flowInPreviousNum = searchProvinceToTypeNum(flowInProvince,"感染患者");
  		
  		flowOutCurrentNum = flowOutPreviousNum-flowNum;
  		flowInCurrentNum = flowInPreviousNum+flowNum;
@@ -360,20 +362,19 @@ class InfectStatistic {
  	   ProvinceToNumMap.get(flowInProvince).replace(linePart[1],flowInCurrentNum);
  	}
  	
-      
-      /*
+    
+
+	/*
    	 *函数功能：疑似患者流动
    	 *输入参数：
    	 *输出参数：
    	 **/
-     public void suspectedPatientsMove(String linePart[]) {
+     public void suspectedPatientsMove(String lineInformation) {
    		
-
   		//先将每一行的字符串分隔成字符串数组
   		String[] linePart = lineInformation.split(" ");
   		String flowOutProvince = linePart[0];
   		String flowInProvince = linePart[3];
-  		
   		
   		//流动患者数量
   		int flowNum;
@@ -384,7 +385,7 @@ class InfectStatistic {
   		int flowOutPreviousNum,flowInPreviousNum,flowOutCurrentNum,flowInCurrentNum;
   		
   		flowOutPreviousNum = searchProvinceToTypeNum(flowOutProvince,"疑似患者");
-  		flowInPreviousNum = searchProvinceToTypeNum(flowInPreviousNum,"疑似患者");
+  		flowInPreviousNum = searchProvinceToTypeNum(flowInProvince,"疑似患者");
   		
   		flowOutCurrentNum = flowOutPreviousNum-flowNum;
   		flowInCurrentNum = flowInPreviousNum+flowNum;
@@ -399,7 +400,7 @@ class InfectStatistic {
        	 *输入参数：
        	 *输出参数：
        	 **/
-     public void addDeadPatients(String linePart[]) {
+     public void addDeadPatients(String lineInformation) {
     	//先将每一行的字符串分隔成字符串数组
   		String[] linePart = lineInformation.split(" ");
   		String province = linePart[0];
@@ -427,7 +428,7 @@ class InfectStatistic {
           *输入参数：
          *输出参数：
           **/
-     public void addCurePatients(String linePart[]) {
+     public void addCurePatients(String lineInformation) {
     	//先将每一行的字符串分隔成字符串数组
    		String[] linePart = lineInformation.split(" ");
    		String province = linePart[0];
@@ -456,13 +457,11 @@ class InfectStatistic {
                	 *输入参数：
                	 *输出参数：
                	 **/
-      public void suspectedToInfection(String linePart[]) {
+      public void suspectedToInfection(String lineInformation) {
                		
                		//先将每一行的字符串分隔成字符串数组
                		String[] linePart = lineInformation.split(" ");
                		String province = linePart[0];
-               		
-               		
                		
                		//疑似患者确认感染数量
                		int num;
@@ -499,7 +498,7 @@ class InfectStatistic {
     	 *输入参数：
     	 *输出参数：
     	 **/
-         public void suspectedToNormal(String linePart[]) {
+         public void suspectedToNormal(String lineInformation) {
 
         		//先将每一行的字符串分隔成字符串数组
         		String[] linePart = lineInformation.split(" ");
@@ -522,7 +521,7 @@ class InfectStatistic {
         		countryCurrentSuspectedNum = countryPreviousSuspectedNum-num;
         				
         	    ProvinceToNumMap.get(province).replace("疑似患者",currentSuspectedNum);
-        	 ProvinceToNumMap.get("全国").replace("疑似患者",countryCurrentInfectionNum);
+        	 ProvinceToNumMap.get("全国").replace("疑似患者",countryCurrentSuspectedNum);
     	}
 
                    		
@@ -534,81 +533,100 @@ class InfectStatistic {
 	 *输入参数：
 	 *输出参数：
 	 **/
+
 	public void outputData(String path) {
-		FileWriter fileWriter = null;
-		File file = new File(path);
+		
+		
 		try {
+			File file = new File(path);
 			if(!file.exists()) {
-				file.createNewFile(path);
+				file.createNewFile();
 			}
-			fileWriter = new FileWriter(file);
-			BufferedWriter out = new BufferedWriter(fileWriter);
+			FileOutputStream foStream = new FileOutputStream(file);
+			OutputStreamWriter opStream = new OutputStreamWriter(foStream);
+			BufferedWriter writer = new BufferedWriter(opStream);
+			
+			
+			
 			
 			//若-province包含全国或者命令行不包含-province
 			if(commandProvinceList.contains("全国")||commandProvinceList.isEmpty()) {
+				if(typeList.isEmpty()) {
+					typeList.add("ip");
+					typeList.add("sp");
+					typeList.add("cure");
+					typeList.add("dead");
+				}
+				
 				Set<String> thisSet = ProvinceToNumMap.keySet();
-				for(String strKey:thisSet) {		
+				for(String strKey:thisSet) {	
 					HashMap<String,Integer> TypeToNumValue = ProvinceToNumMap.get(strKey);
 					Set<String> set = TypeToNumValue.keySet();
 					for(String integerKey:set) {
-						for(int j=0;j<typeList.size();i++) {						
-							if(typeList.isEmpty()) {
-								typeList.add("ip","sp","cure","dead");
-							}
-							switch(typeList[i]) {
-							case "ip":Integer value = TypeToNumValue.get("感染患者");
-							out.write(strKey+" "+"感染患者"+" "+value+"\n");
-							case "sp":Integer value = TypeToNumValue.get("疑似患者");
-							out.write(strKey+" "+"疑似患者"+" "+value+"\n");
-							case "cure":Integer value = TypeToNumValue.get("治愈");
-							out.write(strKey+" "+"治愈"+" "+value+"\n");
-							case "dead":Integer value = TypeToNumValue.get("死亡");
-							out.write(strKey+" "+"死亡"+" "+value+"\n");
+						for(int j=0;j<typeList.size();j++) {	
+							if(integerKey.equals(typeList.get(j))) {
+							switch(typeList.get(j)) {
+							case "ip":Integer value = TypeToNumValue.get(integerKey);
+							writer.write(strKey+" "+"感染患者"+" "+value+"\n");
+							case "sp":Integer value1 = TypeToNumValue.get(integerKey);
+							writer.write(strKey+" "+"疑似患者"+" "+value1+"\n");
+							case "cure":Integer value2 = TypeToNumValue.get(integerKey);
+							writer.write(strKey+" "+"治愈"+" "+value2+"\n");
+							case "dead":Integer value3 = TypeToNumValue.get(integerKey);
+							writer.write(strKey+" "+"死亡"+" "+value3+"\n");
 							}
 						}	
+							}
 						}
 					}
 				}
-			}
+			
 			else {
 				Set<String> thisSet = ProvinceToNumMap.keySet();
 				for(String strKey:thisSet) {		
 					for(int i=0;i<commandProvinceList.size();i++) {
-						if(strKey.equals(commandProvinceList[i])) {
+						if(strKey.equals(commandProvinceList.get(i))) {
 							HashMap<String,Integer> TypeToNumValue = ProvinceToNumMap.get(strKey);
 						Set<String> set = TypeToNumValue.keySet();
 						for(String integerKey:set) {
 							for(int j=0;j<typeList.size();i++) {
-							switch(typeList[i]) {
-							if(typeList.isEmpty()) {
-								typeList.add("ip","sp","cure","dead");
-							}
-							case "ip":Integer value = TypeToNumValue.get("感染患者");
-							out.write(strKey+" "+"感染患者"+" "+value+"\n");
-							case "sp":Integer value = TypeToNumValue.get("疑似患者");
-							out.write(strKey+" "+"疑似患者"+" "+value+"\n");
-							case "cure":Integer value = TypeToNumValue.get("治愈");
-							out.write(strKey+" "+"治愈"+" "+value+"\n");
-							case "dead":Integer value = TypeToNumValue.get("死亡");
-							out.write(strKey+" "+"死亡"+" "+value+"\n");
-							}
-						}
+								if(typeList.isEmpty()) {
+									typeList.add("ip");
+									typeList.add("sp");
+									typeList.add("cure");
+									typeList.add("dead");
+								}
+								if(integerKey.equals(typeList.get(j))) {
+								switch(typeList.get(j)) {
+								case "ip":Integer value = TypeToNumValue.get("感染患者");
+								writer.write(strKey+" "+"感染患者"+" "+value+"\n");
+								case "sp":Integer value1 = TypeToNumValue.get("疑似患者");
+								writer.write(strKey+" "+"疑似患者"+" "+value1+"\n");
+								case "cure":Integer value2 = TypeToNumValue.get("治愈");
+								writer.write(strKey+" "+"治愈"+" "+value2+"\n");
+								case "dead":Integer value3 = TypeToNumValue.get("死亡");
+								writer.write(strKey+" "+"死亡"+" "+value3+"\n");
+								}
+						}}
 							}
 						}
 					}
 				}
 			} 
-			out.write("// 该文档并非真实数据，仅供测试使用");
+			writer.write("// 该文档并非真实数据，仅供测试使用");
+			writer.close();
 		}catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	
-    public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException, IOException {
        InfectStatistic infectStatistic = new InfectStatistic();
        infectStatistic.analyseCommandLine(args);
        infectStatistic.searchFile(inputAddress);
-       infectStatistic.outputData(outPutAddesss);
+       infectStatistic.outputData(outputAddress);
     }
+   
+    
 }
+ 

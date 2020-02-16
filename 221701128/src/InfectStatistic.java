01bc591f8730eb217c0fe376362404acde754c53
lqi59780808 <41891928+lqi59780808@users.noreply.github.com>
2020-02-16 16:07:50 +0800

    增加-out功能
    保存-out后的输出目录文件路径

--- 221701128/src/InfectStatistic.java
+++ 221701128/src/InfectStatistic.java
@@ -3,6 +3,8 @@ public class InfectStatistic {
 	
 	public static int i;
 	public static String fileDirect;
+	public static String outputFilepath;
+	
 	public static boolean judgeList (String str)
 	{
 		str=str.substring(0,1);
@@ -16,12 +18,24 @@ public class InfectStatistic {
 		{
 			readLog(str);
 		}
+		
+		else if(str[i].equalsIgnoreCase("-out"))
+		{
+			readOutputPath(str);
+		}
+	}
+	
+	public static void readOutputPath (String str[])
+	{
+		i++; //读取-out地址
+		outputFilepath=str[i]; //保存路径
+		System.out.print(outputFilepath);
 	}
 	
 	public static void readLog (String str[])
 	{
 		i++; //读取-log地址
-		fileDirect=str[i]; //保存目录路径
+		fileDirect=str[i]; //保存路径
 		System.out.print(fileDirect);
 	}
 	
@@ -35,6 +49,6 @@ public class InfectStatistic {
     		}
     	}
     	return;
-    }  
+    }
 
 }

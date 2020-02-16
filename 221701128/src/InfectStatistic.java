7ade16443b2edcb102d5dfa2b62fcd27c57be8ab
lqi59780808 <41891928+lqi59780808@users.noreply.github.com>
2020-02-16 16:40:51 +0800

    增加-date读取日期功能
    将-date的参数日期保存在dateTime中

--- 221701128/src/InfectStatistic.java
+++ 221701128/src/InfectStatistic.java
@@ -4,10 +4,11 @@ public class InfectStatistic {
 	public static int i;
 	public static String fileDirect;
 	public static String outputFilepath;
+	public static String dateTime;
 	
 	public static boolean judgeList (String str)
 	{
-		str=str.substring(0,1);
+		str = str.substring(0,1);
 		if(str.equals("-")) return true;
 		else return false;
 	}
@@ -23,20 +24,49 @@ public class InfectStatistic {
 		{
 			readOutputPath(str);
 		}
+		
+		else if(str[i].equalsIgnoreCase("-date"))
+		{
+			readDateTime(str);
+		}
 	}
 	
 	public static void readOutputPath (String str[])
 	{
-		i++; //读取-out地址
-		outputFilepath=str[i]; //保存路径
+		if(i != str.length - 1) 
+		{
+			i++; //读取-out地址
+			outputFilepath = str[i]; //保存路径
+		}
+		
 		System.out.print(outputFilepath);
 	}
 	
 	public static void readLog (String str[])
 	{
-		i++; //读取-log地址
-		fileDirect=str[i]; //保存路径
+		if(i != str.length - 1) 
+		{
+			i++; //读取-log地址
+			fileDirect = str[i]; //保存路径
+		}
+		
 		System.out.print(fileDirect);
+	}
+	
+	public static void readDateTime (String str[])
+	{
+		if(i == str.length - 1 || str[i+1].substring(0,1).equals("-"))
+		{
+			dateTime = "latest";
+		}
+		
+		else
+		{
+			i++;
+			dateTime = str[i];
+		}
+		
+		System.out.print(dateTime);
 	}
 	
 	public static void main(String args[])

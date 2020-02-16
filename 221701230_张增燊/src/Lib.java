6937a806892b98296154b3b4371b1cc9a534f951
ZZS822 <50981213+ZZS822@users.noreply.github.com>
2020-02-16 13:40:57 +0800

    对Lib的更新，加入了要用到的数据结构
    Lib里有两个类，一个是Province，一个是Lib类。Province是用来记录各省的情况，Lib是记录每天的情况，lib里有一个Province组成的Vector

--- 221701230_张增燊/src/Lib.java
+++ 221701230_张增燊/src/Lib.java
@@ -1,1 +1,153 @@
+import java.util.*;
 
+class Province {
+	
+	private String name;
+	private int ip;
+	private int sp;
+	private int cure;
+	private int dead;
+	
+	public Province(String name)//构造函数
+	{
+		this.name=name;
+		ip=0;
+		sp=0;
+		cure=0;
+		dead=0;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getIp()
+	{
+		return ip;
+	}
+	
+	public int getSp()
+	{
+		return sp;
+	}
+	
+	public int getCure()
+	{
+		return cure;
+	}
+	
+	public int getDead()
+	{
+		return dead;
+	}
+	
+	public void addIp(int x)//增加感染者
+	{
+		ip=ip+x;
+	}
+	
+	public void addSp(int x)//增加疑似患者
+	{
+		sp=sp+x;
+	}
+	
+	public void addCure(int x)
+	{
+		cure=cure+x;
+	}
+	
+	public void addDead(int x)
+	{
+		dead=dead+x;
+	}
+	
+	public void moveIp(int x)
+	{
+		ip=ip-x;
+		if(ip<0)
+			ip=0;
+	}
+	
+	public void moveSp(int x)
+	{
+		sp=sp-x;
+		if(sp<0)
+			sp=0;
+	}
+	
+	public String ToString()
+	{
+		return name+" 感染患者"+ip+"人 疑似患者"+sp
+				+"人 治愈"+cure+"人 死亡"+dead+"人\n";
+	}
+	
+	public String outIp()
+	{
+		return "感染患者"+ip+"人";
+	}
+	
+	public String outSp()
+	{
+		return "疑似患者"+sp+"人";
+	}
+	
+	public String outCure()
+	{
+		return "治愈"+cure+"人";
+	}
+	
+	public String outDead()
+	{
+		return "死亡"+dead+"人";
+	}
+}
+
+public class Lib {
+	String date;
+	Vector<Province> provinces;
+	
+	public Lib(String date)
+	{
+		this.date=date;
+		provinces=new Vector<Province>();
+		
+		provinces.addElement(new Province("全国"));
+		provinces.addElement(new Province("安徽"));
+		provinces.addElement(new Province("北京"));
+		provinces.addElement(new Province("重庆"));
+		provinces.addElement(new Province("福建"));
+		provinces.addElement(new Province("甘肃"));
+		provinces.addElement(new Province("广东"));
+		provinces.addElement(new Province("广西"));
+		provinces.addElement(new Province("贵州"));
+		provinces.addElement(new Province("海南"));
+		provinces.addElement(new Province("河北"));
+		provinces.addElement(new Province("河南"));
+		provinces.addElement(new Province("黑龙江"));
+		provinces.addElement(new Province("湖北"));
+		provinces.addElement(new Province("湖南"));
+		provinces.addElement(new Province("吉林"));
+		provinces.addElement(new Province("江苏"));
+		provinces.addElement(new Province("江西"));
+		provinces.addElement(new Province("辽宁"));
+		provinces.addElement(new Province("内蒙古"));
+		provinces.addElement(new Province("宁夏"));
+		provinces.addElement(new Province("青海"));
+		provinces.addElement(new Province("山东"));
+		provinces.addElement(new Province("山西"));
+		provinces.addElement(new Province("陕西"));
+		provinces.addElement(new Province("上海"));
+		provinces.addElement(new Province("四川"));
+		provinces.addElement(new Province("天津"));
+		provinces.addElement(new Province("西藏"));
+		provinces.addElement(new Province("新疆"));
+		provinces.addElement(new Province("云南"));
+		provinces.addElement(new Province("浙江"));
+	}
+	
+	public String getDate()
+	{
+		return date;
+	}
+}

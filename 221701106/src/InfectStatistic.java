898da713bb1ae714e19e884174189717e21fc962
221701106lxy <61062063+221701106lxy@users.noreply.github.com>
2020-02-17 23:41:35 +0800

    today's coding end
    今日的编码结束，剩下的工作明天接着做。

--- 221701106/src/InfectStatistic.java
+++ 221701106/src/InfectStatistic.java
@@ -7,6 +7,7 @@
  * @since xxx
  */
 import java.io.*;
+import java.util.regex.Pattern;
 class InfectStatistic {
     public static void main(String args[]) throws IOException {
     	for (int i=0;i<args.length;i++) {		//循环读取命令行参数
@@ -15,13 +16,28 @@ class InfectStatistic {
 			String outpath=null;				//输入文件路径
 			if (args[i].equals("-log")) {
 				logpath=args[i+1];				//-log参数的下一个参数即为日志文件路径名
-				System.out.println(logpath);
+				//System.out.println(logpath);
 				LogProcess(logpath);			//对-log参数进行处理
 			}
 			if (args[i].equals("-out")) {
 				outpath=args[i+1];				//-out参数的下一个参数即为输出文件路径名
+				outProcess(outpath);			//对-out参数进行处理
 			}
 		}
+    }
+    public static void LogContentHandle(String lineText) {
+    	String match1="(\\S+) 新增 感染患者 (\\d+)人";
+    	String match2="(\\S+) 新增 疑似患者 (\\d+)人";
+    	String match3="(\\S+) 治愈 (\\d+)人";
+    	String match4="(\\S+) 死亡 (\\d+)人";
+    	String match5="(\\S+) 感染患者 流入 (\\S+) (\\d+)人";
+    	String match6="(\\S+) 疑似患者 流入 (\\S+) (\\d+)人";
+    	String match7="(\\S+) 疑似患者 确诊感染 (\\d+)人";
+    	String match8="(\\S+) 排除 疑似患者(\\d+)人";
+    	boolean isMatch1,isMatch2,isMatch3,isMatch4,isMatch5,isMatch6,isMatch7,isMatch8;
+    	if (isMatch1=Pattern.matches(match1,lineText)) {
+    		
+    	}
     }
     public static void LogProcess(String logpath) throws IOException {
     	File logfilepath=new File(logpath);
@@ -38,5 +54,8 @@ class InfectStatistic {
 			}
 			buff.close();
 		}
+    }
+    public static void OutProcess(String outpath) throws IOException {		//输出到指定文件中
+    	File outfilepath=new File(outpath);
     }
 }

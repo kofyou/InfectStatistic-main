3562952f8c53fefef8455f5df87686f73424617c
Kylinseph <52260506@qq.com>
2020-02-17 23:13:28 +0800

    feat: add function filter log by date

--- 081700430/src/InfectStatistic.java
+++ 081700430/src/InfectStatistic.java
@@ -1,4 +1,7 @@
 import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,8 +15,10 @@ import java.util.Map;
  * @since 2020/2/9
  */
 class InfectStatistic {
-    private String log, out, date;
+    private String log, out;
     private String [] type, province;
+    private Date date;
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
     private final int typeNum = 4, provinceNum = 34;
 
     private void init(String[] args) {
@@ -29,7 +34,13 @@ class InfectStatistic {
                         this.out = args[++i];
                         break;
                     case "-date" :
-                        this.date = args[++i];
+                        try {
+                            this.date = sdf.parse(args[++i]);
+                        }
+                        catch (ParseException pE) {
+                            System.out.println("请输入正确的日期！");
+                            System.exit(-1);
+                        }
                         break;
                     case "-type" :
                         i++;
@@ -55,11 +66,50 @@ class InfectStatistic {
         }
     }
 
-/*    private void dealLogs() {
+    private void dealLogs() {
+        System.out.println(log + " ");/* dbg:调试查看读取文件是否正确 */
         File file = new File(log);
-        if (file.isDirectory()) {
-
+        if (file.isDirectory() && file.list() != null) {
+            File[] files = file.listFiles();
+            if (files != null)
+            for (File aFile : files) {
+                String[] splitName = aFile.getName().split("\\.");
+                if (aFile.isFile() && splitName.length > 2 && splitName[1].equals("log")) {
+                  //  System.out.println(aFile.getName() + " " + splitName[0] + " " +splitName[1]+" "+splitName[2]);/* dbg:调试查看读取文件是否正确 */
+                    if (date != null) {
+                        try {
+                            Date fileDate = sdf.parse(splitName[0]);
+                            if (fileDate.before(date) || fileDate.equals(date)) {
+                                //System.out.println(aFile.getName() + " " + splitName[0] + " " +splitName[1]+" "+splitName[2]);
+                                caculateLogs(aFile);
+                            }
+                        }
+                        catch (ParseException pE) {
+                        }
+                    }
+                    else {
+                        //System.out.println(aFile.getName() + " " + splitName[0] + " " +splitName[1]+" "+splitName[2]);
+                        caculateLogs(aFile);
+                    }
+                }
+            }
         }
+    }
+
+    public void caculateLogs(File file) {
+
+    }
+
+    public void outFile() {
+        File file = new File(out);
+        try {
+            if (!file.exists()) {
+
+            }
+        }
+        catch (Exception e) {
+        }
+
     }
 
     private void showPara() {
@@ -67,11 +117,12 @@ class InfectStatistic {
         for (int i = 0; i < typeNum; i++) {
 
         }
-    } */
+    }
 
     public static void main(String[] args) {
         InfectStatistic infect = new InfectStatistic();
         infect.init(args);
+        infect.dealLogs();
         System.out.println("helloworld");
     }
 

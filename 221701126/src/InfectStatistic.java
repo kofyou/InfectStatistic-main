a7b8f7a01874cd5c380e64aa2098647fd2b8700c
唐靖钧 <1195669260@qq.com>
2020-02-18 06:50:26 +0800

    按照代码规范整改代码

--- 221701126/src/InfectStatistic.java
+++ 221701126/src/InfectStatistic.java
@@ -26,40 +26,42 @@ import javax.print.attribute.standard.OutputDeviceAssigned;
 
 /**
  * InfectStatistic
- * TODO
+ * TODO 
  *
  * @author 221701126_唐靖钧
  * @version 1.0
  * @since 2020/2/8
- */
+ */ 
 class InfectStatistic {
 	public static String inputPath = "C:\\Users\\Peter\\Documents\\GitHub\\InfectStatistic-main\\221701126\\log\\";
 	public static String outputPath = "C:\\Users\\Peter\\Documents\\GitHub\\InfectStatistic-main\\221701126\\result\\out.txt";
 	public static String targetDate = "";
 	public static ArrayList<String> provinceArray = new ArrayList<String>();
 	public static Map<String, Province> map = new HashMap<String, Province>();
-	public static Province country = new Province();//用于统计全国人数
+	public static Province country = new Province();/*用于统计全国人数*/
 	public static boolean ip = false;
 	public static boolean sp = false;
-	public static boolean cure = false;
+	public static boolean cure = false; 
 	public static boolean dead = false;
-	public static ArrayList<String> typeItem = new ArrayList<String>();
-	public static ArrayList<String> provinceItem = new ArrayList<String>();
+	public static ArrayList<String> typeItem = new ArrayList<String>();/*记录type命令所带的选项*/
+	public static ArrayList<String> provinceItem = new ArrayList<String>();/*记录province命令所带的选项*/
 	public static String[] province = {"全国", "安徽", "北京", "重庆", "福建", "甘肃", "广东", "广西", "贵州", "海南", "河北",
 			"河南", "黑龙江", "湖北", "湖南", "吉林", "江苏", "江西", "辽宁", "内蒙古", "宁夏", "青海", "山东", "山西", "陕西",
 			"上海", "四川", "天津", "西藏", "新疆", "云南", "浙江"
 	};
 	
-	//获取目录下日期最大的文件名
+	/*
+	 * 获取目录下日期最大的文件名
+	 */
 	public static String getMaxDate() {
 		String maxDate = "0";
 		File file = new File(inputPath);
 		if(file.exists()) {
 			String[] fileNames = file.list(); // 获得目录下的所有文件的文件名
 			for(String fileName : fileNames) {
-				//去后缀
-				fileName = fileName.substring(0, fileName.indexOf('.'));
-				//日期比较
+				fileName = fileName.substring(0, fileName.indexOf('.'));//去后缀
+				
+				//日期 比较
 				if(fileName.compareTo(maxDate) > 0) {
 					maxDate = fileName;
 				}
@@ -69,7 +71,9 @@ class InfectStatistic {
 		return maxDate;
 	}
 	
-	//将命令行参数转换成相应变量存储起来
+	/*
+	 * 将命令行参数转换成相应变量存储起来
+	 */
 	public static void solveArgs(String[] args) {
 		int i = 0;
 		int pos = 1;
@@ -137,7 +141,9 @@ class InfectStatistic {
 	}
 	
 	
-	//打印一个省的信息
+	/*
+	 * 打印一个省的信息
+	 */
 	public static void printTheProvince(String provinceName, OutputStreamWriter osw) {
 		try {
 			
@@ -213,7 +219,9 @@ class InfectStatistic {
 		}
 	}
 	
-	//测试打印结果
+	/*
+	 * 测试打印结果
+	 */
 	public static void printResult() {
 		try {
 			File output = new File(outputPath);
@@ -236,12 +244,12 @@ class InfectStatistic {
 			osw.flush();
 			osw.close();
 		} catch (IOException e) {
-			// TODO: handle exception
-			e.printStackTrace();
 		}
 	}
 	
-	//获取各行的人数
+	/*
+	 * 获取各行的人数
+	 */
 	public static int getNumber(String[] information) {
 		//获取人数
 		String numString = information[information.length - 1];
@@ -251,7 +259,9 @@ class InfectStatistic {
 		return number;
 	}
 	
-	//处理待处理文件的每一个文件
+	/*
+	 * 处理待处理文件的每一个文件
+	 */
 	public static void solveEveryFile(Vector<String> toHandleDate) {
 		
 		
@@ -263,10 +273,8 @@ class InfectStatistic {
 	    		File file = new File(dateFile);
 	    		InputStreamReader inputReader = new InputStreamReader(new FileInputStream(file), "UTF-8");
 	    		BufferedReader bf = new BufferedReader(inputReader);
-	    		
-	    		
-	    		
 	    		String str;
+	    		
 	    		while ((str = bf.readLine()) != null && str.indexOf("//") != 0) {
 	    			//System.out.println(str);
 	    			String[] information = str.split("\\s+");
@@ -401,7 +409,6 @@ class InfectStatistic {
 	    	
 			} 
 	    	catch (IOException  e) {
-				// TODO: handle exception
 	    		e.printStackTrace();
 			}
 		}
@@ -459,13 +466,13 @@ class InfectStatistic {
     }
 }
 
-
+/*记录一个省的各项数据*/
 class Province{
-	String name;//省名
-	int infect;//感染人数
-	int seeming;//疑似人数
-	int dead;//死亡人数
-	int cured;//治愈人数
+	String name; /*省名*/
+	int infect; /*感染人数*/
+	int seeming; /*疑似人数*/
+	int dead; /*死亡人数*/
+	int cured; /*治愈人数*/
 	
 	public Province() {
 		infect = seeming = dead = cured = 0;

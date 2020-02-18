181b133d08245532de600ecded7e387b1211a1f7
LateComerYC <1412617067@qq.com>
2020-02-18 14:19:08 +0800

    添加错误输入提示

--- 221701215/src/InfectStatistic.java
+++ 221701215/src/InfectStatistic.java
@@ -1,25 +1,34 @@
-import java.io.InputStream;
-
 /**
  * InfectStatistic
  * TODO
-@@ -6,9 +8,235 @@
- * @version xxx
- * @since xxx
+ *
+ * @author yangmingwei
+ * @version 1.0
+ * @since 2020/2/15
  */
 import java.util.Vector;
+import java.io.InputStream;
+import java.text.Collator;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Locale;
 import java.io.*;
 public class InfectStatistic {
     public static void main(String[] args) {
-    	for(int i = 0;i<args.length;i++) System.out.println(args[i]);
-    	DocFormatter doc=new DocFormatter(args);
-    	Excute excute=new Excute(doc);
-    	excute.run();
-        System.out.println("helloworld");
+    	DocFormatter doc=new DocFormatter(args);  
+    	Excute excute=new Excute(doc);           
+    	excute.run();							
     }
+
+
 }
+
 class DocFormatter{
 	private String[] cmd;
 	private int logPathInd;
@@ -27,11 +36,15 @@ class DocFormatter{
 	private int dateInd;
 	private int typeInd;
 	private int provinceInd;
+	private Vector<String> pros;
 	DocFormatter(String[] arg){
-       cmd=arg;
-       exchange();
+		pros=new Vector<>();
+		init_pro(pros);
+		cmd=arg;
+        exchange();
 	}
-	public void exchange() {
+	
+	public void exchange() {           
 		dateInd=-1;
 		typeInd=-1;
 		provinceInd=-1;
@@ -43,108 +56,152 @@ class DocFormatter{
 			if(cmd[i].equals("-type"))  typeInd=++i;
 		}
 	}
-	public String getlogPath() {
+	private void init_pro(Vector<String> vec) {                      
+		String[] pros= {"北京","天津","上海","重庆","河北","山西","辽宁","吉林","黑龙江"
+				,"江苏","浙江","安徽","福建","江西","山东","河南","湖北","湖南","广东"
+				,"海南","四川","贵州","云南","陕西","甘肃","青海","台湾","内蒙古","广西"
+				,"西藏","宁夏","新疆","香港","澳门","全国"};
+		for(int i=0;i<35;i++) {
+			vec.add(pros[i]);
+		}
+	}
+	
+	public String getlogPath() {							
 		return cmd[logPathInd];
 	}
-	public String getoutPath() {
+	
+	public String getoutPath() {						
 		return cmd[outPathInd];
 	}
-	public String getDate() {
+	/*获取日期*/
+	public String getDate() {               
 		if(dateInd==-1) return "Latest";
 		return cmd[dateInd];
 	}
-	public Vector getType() {	
+	/*获取-type的参数值*/
+	public Vector<String> getType() {						
 		Vector<String> type= new Vector<String>();
+		Vector<String> vec =new Vector<>();
+		vec.add("-log");vec.add("-out");
+		vec.add("-date");vec.add("-province");
 		if(typeInd!=-1) {
 			int i=typeInd;
-			while( i<cmd.length &&(cmd[i]!="-log"||cmd[i]!="-out"||cmd[i]!="-province"||cmd[i]!="-date")) {
-				type.add(cmd[i]);
+			while( i<cmd.length &&(!vec.contains(cmd[i]))) {	
+				type.add(cmd[i]);		
 				i++;
 			}	
 		}
 		return type;
 	}
-	public Vector<String> getPro() {
+	/*获取-province的参数值,并检验省份的输入是否合法*/
+	public Vector<String> getPro() {				
 		Vector<String> vec= new Vector<String>();
+		Vector<String> vecs =new Vector<>();
+		vecs.add("-log");vecs.add("-out");
+		vecs.add("-date");vecs.add("-type");
 		if(provinceInd!=-1) {
 			int i=provinceInd;
-			while( i<cmd.length &&(!cmd[i].equals("-log")||cmd[i]!="-out"||cmd[i]!="-type"||cmd[i]!="-date")) {
-				vec.add(cmd[i]);
+			while( i<cmd.length &&(!vecs.contains(cmd[i]))) {	
+				if(!pros.contains(cmd[i]))  {
+					System.out.println("省份输入错误，为默认输出!");
+					vec.clear();
+					return vec;
+				}
+				vec.add(cmd[i]);	
 				i++;
 			}	
 		}
 		return vec;
 	}	
 }
-class Excute{
-	private HashMap<String,Status> map;
+
+ class Excute{
+	/*使用map保存疫情信息，ke为省份名称，value为Status记录人数*/
+	private Map<String,Status> map;						
 	private DocFormatter Formatter;
-	private boolean tyChecked[];
+	private String[] tyChecked;
 	private boolean TyChecked;
 	private boolean ProChecked;
+	private Status Total;
 	private Vector<String> pro;
 	public Excute(DocFormatter d) {
 		Formatter =d;
 		ProChecked=false;
 		TyChecked=false;
-		tyChecked= new boolean[]{false,false,false,false};
+		tyChecked= new String[]{"","","",""};
+		Total =new Status("全国");		
 		pro=new Vector<>();
-		map=new HashMap<>();
+		map=new HashMap<String,Status>();
+		map.put("全国",Total);
 	}
-	public void run() {
-		for(int i=0;i<Formatter.getType().size();i++) {
+	public void run() {                                    
+		for(int i=0;i<Formatter.getType().size();i++) {                   //按-type参数初始化输出格式，并检验-type参数的合法性
 			TyChecked=true;
-			if(Formatter.getType().get(i).equals("ip")) tyChecked[0]=true;
-			if(Formatter.getType().get(i).equals("sp")) tyChecked[1]=true;
-			if(Formatter.getType().get(i).equals("dead")) tyChecked[2]=true;
-			if(Formatter.getType().get(i).equals("cure")) tyChecked[3]=true;
+			if(Formatter.getType().get(i).equals("ip")) tyChecked[i]="感染患者";
+			else if(Formatter.getType().get(i).equals("sp")) tyChecked[i]="疑似患者";
+			else if(Formatter.getType().get(i).equals("dead")) tyChecked[i]="死亡";
+			else if(Formatter.getType().get(i).equals("cure")) tyChecked[i]="治愈";
+			else {
+				System.out.println("测试"+Formatter.getType().get(i));
+				System.out.println("-type参数错误，请输入ip、sp、dead或者cure！");
+				return ;
+			}
+		
 		}
-		for(int i=0;i<Formatter.getPro().size();i++) {
+		for(int i=0;i<Formatter.getPro().size();i++) {            
 			ProChecked=true;
 			pro.add(Formatter.getPro().get(i));
+			
 		}
 		try{
-			String date=Formatter.getDate();
+			String date=Formatter.getDate();                                     //读取文件
 			String source=Formatter.getlogPath();
-			openFile();
+			Vector<String> vec =getFiles(date,source);							//根据日期返回要打开的文件
+			if(vec.size()<=0) { 
+					System.out.println("打开文件错误，请检查入是否正确！");
+					return ;
+				}
+			for(int i=0;i<vec.size();i++) {
+				openFile(vec.get(i));
+			}
+			
 		}
 		catch(IOException e) {
-			
+			System.out.println("打开文件错误，请检查入是否正确！");
+			return ;
 		}
 		try {
 			String dest=Formatter.getoutPath();
-			WriteFile(dest);
+			WriteFile(dest);                                       //写入文件
 		}
 		catch(IOException e) {
-			
+			System.out.println("写入文件错误，请检查输入是否正确！");
 		}
 	}
-	private String openFile() throws IOException  {
-		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("a.txt"),"UTF-8"));
+	private String openFile(String path) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path),"UTF-8"));
 		String line=null;
-		while((line=br.readLine())!=null) {		
-			String[] arr = line.split(" "); // ��,�ָ�
-			
-			
+		while((line=br.readLine())!=null) {								//按行读取文件
+			String[] arr = line.split(" "); 							// 用  分割文件的每一行，并保存在arr数组中
 				if(!map.containsKey(arr[0]) ) {
-					map.put(arr[0],new Status(arr[0]));
+					map.put(arr[0],new Status(arr[0]));					//根据每行开头的省份创建Status类并放入map中
 				}
-				Status s=map.get(arr[0]);				
-				if(arr[1].equals("����")) {
-					if(arr[2].equals("��Ⱦ����"))  addIP(s,arr[3]);
+				Status s=map.get(arr[0]);								//读取arr，检查是否匹配“新增”，“死亡”等情况，修改Status相应变量
+				if(arr[1].equals("新增")) {
+					if(arr[2].equals("感染患者"))  addIP(s,arr[3]);
 					else  addSP(s,arr[3]);
 				}
-				else if(arr[1].equals("����")) {
+				else if(arr[1].equals("死亡")) {
 					addDead(s,arr[2]);
 				}
-				else if(arr[1].equals("����")) {
+				else if(arr[1].equals("治愈")) {
 					addCure(s,arr[2]);
 				}
-				else if(arr[1].equals("�ų�")) {
+				else if(arr[1].equals("排除")) {
 					delSp(s,arr[3]);
 				}
-				else if(arr[1].equals("���ƻ���")) {
-					if(arr[2].equals("ȷ���Ⱦ"))  spToip(s,arr[3]);
+				else if(arr[1].equals("疑似患者")) {
+					if(arr[2].equals("确诊感染"))  spToip(s,arr[3]);
 					else {
 						if(!map.containsKey(arr[3]) ) {
 							map.put(arr[3],new Status(arr[3]));
@@ -153,7 +210,7 @@ class Excute{
 						spRunTo(s,t,arr[4]);
 					} 
 				}
-				else if(arr[1].equals("��Ⱦ����")){   
+				else if(arr[1].equals("感染患者")){   
 					if(!map.containsKey(arr[3]) ) {
 						map.put(arr[3],new Status(arr[3]));
 					}
@@ -162,31 +219,67 @@ class Excute{
 				else {}
 			
 		}
-		br.close();//�ر��ļ�
-         return "";
+		MakeSumUp();
+		br.close();					//关闭文件
+        return "";
 	};
-	private void WriteFile(String dest) throws IOException{
-		OutputStream os = new FileOutputStream("output.txt");
+	/*将map写入文件,根据docformatter参数，写入指定文件中*/
+	private void WriteFile(String dest) throws IOException{			
+		OutputStream os = new FileOutputStream(dest);
 		PrintWriter pw=new PrintWriter(os);
-		for(String key : map.keySet()){
+		if(ProChecked) {                        			  //检查是否有指定身份
+			for(int i=0;i<pro.size();i++) {        			 //补充缺漏
+				String s=pro.get(i);
+				if(!map.containsKey(s))
+					map.put(s, new Status(s));
+			}
+		}
+		Set<String> set=SortSet(map.keySet());
+		for(String key : set){
 			Status status=map.get(key);
-			if(ProChecked) {                        //����Ƿ���ָ�����
-				if(!pro.contains(key)) continue;         //��ָ����ݣ�continue
+			if(key.equals("//")) continue;
+			if(ProChecked) {                        			//检查是否有指定身份
+				if(!pro.contains(key)) continue;        		 //非指定身份，continue
 			}
 			
 			if(TyChecked) {
 				pw.print(key);
-				if(tyChecked[0])   pw.print(" ��Ⱦ����"+status.ip +"��");
-				if(tyChecked[1])   pw.print(" ���ƻ���"+status.sp +"��");
-				if(tyChecked[2])   pw.print(" ����"+status.cure +"��");
-				if(tyChecked[3])   pw.print(" ����"+status.dead +"��");
+				for(int i=0;i<4;i++) {
+					if(tyChecked[i].equals("感染患者")) pw.print(" 感染患者"+status.ip +"人");
+					else if(tyChecked[i].equals("疑似患者"))   pw.print(" 疑似患者"+status.sp +"人");
+					else if(tyChecked[i].equals("治愈"))   pw.print(" 治愈"+status.cure +"人");
+					else if(tyChecked[i].equals("死亡"))   pw.print(" 死亡"+status.dead +"人");
+					else 					{}
+					
+				};
+				   
+			    pw.print("\n");
 			}
 			else
-		        pw.print(key +" ��Ⱦ����"+status.ip+"�� ���ƻ���"+status.sp+
-		 		   "�� ����"+status.cure+"�� ����"+status.dead+"�� \n");
+		        pw.print(key +" 感染患者"+status.ip+"人 疑似患者"+status.sp+
+		 		   "人 治愈"+status.cure+"人 死亡"+status.dead+"人 \n");
 		}
 		pw.close();
 	}
+	private Vector<String> getFiles(String date,String source){
+		Vector<String> vec =new Vector<>();
+		String basePath=source;
+		String[] list=new File(basePath).list();
+		Vector<String> dates=new Vector<>();
+		for(int i=0;i<list.length;i++) dates.add(list[i]);
+		dates.add(date+".log.txt");
+		Collections.sort(dates);
+		int index=dates.lastIndexOf(date+".log.txt");
+		if(index==dates.size() - 1&&!(dates.get(index).equals(dates.get(index -1)))) {
+			return vec;
+		}
+		for(int i=0;i<dates.lastIndexOf(date+".log.txt");i++) {
+			vec.add(source+dates.get(i));
+		}
+		return vec;
+	}
+
+		
 	private void addIP(Status s,String n) {
 		int num=Integer.parseInt(n.substring(0, n.length()-1));
 		s.ip+=num;
@@ -225,9 +318,39 @@ class Excute{
 		s.sp-=num;
 	}
 	private void add(Status s,Status t) {
-		
+		s.ip+=t.ip;
+		s.sp+=t.sp;
+		s.dead+=t.dead;
+		s.cure+=t.cure;
 	}
-	class Status{
+	private void MakeSumUp() {				//统计全国
+		map.get("全国").cure=0;
+		map.get("全国").ip=0;
+		map.get("全国").sp=0;
+		map.get("全国").dead=0;
+		
+		for(String key : map.keySet()) {
+			if(key.equals("全国")) continue;
+				add(Total,map.get(key));
+		}
+	}
+	public Set<String> SortSet(Set<String> set){
+		 Set<String> sortSet = new TreeSet<String>(new MapKeyComparator());
+		 sortSet.addAll(set);
+		 return sortSet;
+	}
+	public static Map<String,Status> sortMapByKey(Map<String,Status> map) {
+			if (map == null || map.isEmpty()) {
+					return null;
+				}
+			
+			Map<String, Status> sortMap = new TreeMap<String, Status>(
+			new MapKeyComparator());
+			sortMap.putAll(map);
+			return sortMap;
+			}
+	/*封装ip，sp，cure，dead，省份名。*/
+	class Status{					
 		private String pro;
 		private int ip;
 		private int sp;
@@ -239,3 +362,14 @@ class Excute{
 		}
 	}
 }
+/*比较器，按拼音顺序返回，“全国”为最优先值*/
+class MapKeyComparator implements Comparator<String>{
+		public int compare(String str1, String str2) {
+			if(str1.equals("全国")) return -99;
+			if(str2.equals("全国")) return 99;	
+			Collator instance = Collator.getInstance(Locale.CHINA);
+			return instance.compare(str1,str2);
+        
+          }
+
+} 

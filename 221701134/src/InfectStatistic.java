c99f4de07f9152f6bd4e1c75ed4bb1f66e67ff30
VisionWong <302668321@qq.com>
2020-02-18 13:26:26 +0800

    完成221701134文件夹的创建
    - 完成了codestyle.md
    - 完成了.gitignore

---
+++ 221701134/src/InfectStatistic.java
@@ -0,0 +1,14 @@
+/**
+ * InfectStatistic
+ * TODO
+ *
+ * @author xxx
+ * @version xxx
+ * @since xxx
+ */
+class InfectStatistic {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        
+    }
+}

a4ca55a7ed78fe0ac2f142d50835cc801080a2b2
VegetableFriend <43236873+VegetableFriend@users.noreply.github.com>
2020-02-19 02:16:53 +0800

    完成对文本的解析 并输出简单的结果

--- example/src/InfectStatistic.cpp
+++ example/src/InfectStatistic.cpp
@@ -24,25 +24,18 @@ enum Action {
     ///增加疑似
     increase_doubt,
 
-    ///确诊流入
-    sure_transfer,
+    ///减少确诊
+    decrease_sure,
 
-    ///疑似流入
-    doubt_transfer,
+    ///减少疑似
+    decrease_doubt,
 
     ///死亡
     dead,
 
     ///治愈
     cure,
-
-    ///疑似变确诊
-    doubt_to_sure,
-
-    ///排除疑似
-    doubt_remove
 };
-
 
 ///某省疫情信息
 class InfectInfo {
@@ -51,16 +44,16 @@ public:
     bool exist = false;
 
     ///疑似人数
-    int doubt_count;
+    int doubt_count = 0;
 
     ///确诊人数
-    int sure_count;
+    int sure_count = 0;
 
     ///治愈人数
-    int cure_count;
+    int cure_count = 0;
 
     ///死亡人数
-    int dead_count;
+    int dead_count = 0;
 
 private:
 
@@ -105,8 +98,37 @@ public:
     }
 };
 
-///静态数据成员的初始化 应在类外进行 需特别注意 
+///静态数据成员的初始化 应在类外进行 需特别注意
 unordered_map<string, InfectInfo> BaseData::province_keys {};
+
+///工程操作类
+class Command {
+public:
+    static void ActionWith(Action action, int count, string province) {
+        switch (action) {
+            case dead:
+                BaseData::province_keys[province].dead_count += count;
+                BaseData::province_keys[province].sure_count -= count;
+                break;
+            case cure:
+                BaseData::province_keys[province].cure_count += count;
+                BaseData::province_keys[province].sure_count -= count;
+                break;
+            case decrease_sure:
+                BaseData::province_keys[province].sure_count -= count;
+                break;
+            case decrease_doubt:
+                BaseData::province_keys[province].doubt_count -= count;
+                break;
+            case increase_sure:
+                BaseData::province_keys[province].sure_count += count;
+                break;
+            case increase_doubt:
+                BaseData::province_keys[province].doubt_count += count;
+                break;
+        }
+    }
+};
 
 ///工程工具类
 class Tool {
@@ -120,6 +142,7 @@ public:
         input_stream.open("/Users/vegetablefriend/Desktop/InfectStatistic-main/example/log/2020-01-22.log.txt");
 
         while(getline(input_stream, data)) {
+            if (data.find("//") != -1) break;
             getInfoFromString(data);
         }
     }
@@ -129,7 +152,10 @@ public:
         unordered_map<string, InfectInfo>::iterator it;
 
         for(it = BaseData::province_keys.begin(); it != BaseData::province_keys.end() ; it++) {
-            cout << it->second.cure_count << endl;
+            if (it->second.exist) {
+                cout << it->first + " 感染患者" + to_string(it->second.sure_count) + "人 疑似患者" + to_string(it->second.doubt_count) +
+                "人 治愈" + to_string(it->second.cure_count) + "人 死亡" + to_string(it->second.dead_count) + "人" << endl;
+            }
         }
 
     }
@@ -142,12 +168,72 @@ private:
 
         ///此处获取第一个省份
         string province = data.substr(first_index, space_index);
+        data = data.substr(space_index + 1, data.size());
 
         ///注册该省信息
         BaseData::province_keys[province].exist = true;
-        BaseData::province_keys[province].cure_count = 100;
 
-        ///此处获取操作
+        ///此处获取该行信息的人数
+        int last_space = data.find_last_of(' ');
+        int person_index = data.find_last_of("人");
+        string count_string = data.substr(last_space, person_index - last_space - 2);
+        int count = stoi(count_string);
+        data = data.substr(0, last_space);
+
+        ///此处获取该省对应的动作 action
+        Action action;
+
+        space_index = data.find_first_of(' ');
+        string op = data.substr(0, space_index);
+
+        ///根据获得的情况来作出对应的操作
+        if(op == "死亡") {
+            action = dead;
+        } else if (op == "治愈") {
+            action = cure;
+        } else if (op == "疑似患者") {
+
+            ///首先执行一次疑似患者减少的操作
+            action = decrease_doubt;
+            Command::ActionWith(action, count, province);
+
+            ///再判断是确诊了 还是迁出了
+            last_space = data.find_last_of(' ');
+            string str = data.substr(last_space + 1);
+            if(str == "确诊感染") {
+                action = increase_sure;
+            } else {
+                action = increase_doubt;
+                province = str;
+            }
+
+        } else if (op == "排除") {
+            action = decrease_doubt;
+        } else if (op == "新增") {
+            last_space = data.find_last_of(' ');
+            string str = data.substr(last_space + 1);
+
+            if(str == "感染患者") {
+                action = increase_sure;
+            } else {
+                action = increase_doubt;
+            }
+
+        } else if (op == "感染患者") {
+
+            action = decrease_sure;
+            Command::ActionWith(action, count, province);
+
+            last_space = data.find_last_of(' ');
+            string str = data.substr(last_space + 1);
+            action = increase_sure;
+            province = str;
+        }
+
+        Command::ActionWith(action, count, province);
+
+        cout << "--------------------------------------" << endl;
+        outputResult();
     }
 };
 
@@ -156,14 +242,13 @@ class Application {
 public:
     static void run(int argc, char* argv[]) {
 
-        //BaseData::init();
+        BaseData::init();
 
-        //Tool::readDataFromFiles();
+        Tool::readDataFromFiles();
 
-        //Tool::outputResult();
+        Tool::outputResult();
     }
 };
-
 
 //MARK: 程序入口
 /** params:

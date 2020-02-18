3e5dd97de3bf0e201308bba8a172b8b986874a18
GaogaoHub <793744234@qq.com>
2020-02-18 19:40:04 +0800

    feat: finish and check all function of ListCommand

--- 221701306/src/InfectStatistic.java
+++ 221701306/src/InfectStatistic.java
@@ -1,6 +1,9 @@
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static org.junit.Assert.assertNotNull;
+
 import java.io.File;
 import org.junit.Test;
 
@@ -18,7 +21,9 @@ import java.lang.String;
 public class InfectStatistic {
     public static void main(String[] args) {
         ListCommand aListCommand=new ListCommand(args);
-        aListCommand.checkCommand();
+        Statistic aStatistic =new Statistic();
+        aListCommand.checkCommand(aStatistic);
+        LogFiles aLogFiles=new LogFiles(aListCommand.arguments[0].value);
         System.out.println("helloworld");
     }
 
@@ -170,10 +175,13 @@ class ListCommand {
         return true; 
     }
 
-    void checkCommand() {
-        for(int i=0;i<5;++i) {
-            arguments[0].checkError();   
+    void checkCommand(Statistic sta) {
+        for(int i=0;i<4;++i) {
+            if(!arguments[0].checkError()) {
+                System.exit(1);
+            }
         }
+        ((ProvinceArgument)arguments[4]).checkError(sta);
     }
 
 }
@@ -208,12 +216,13 @@ class LogArgument extends BaseArgument {
         if (strs.length > 2) {
             value = "*";
         }
-        value += strs[1];
+        value = strs[1];
     }
 
     boolean checkError() {
         // -log只有一个参数值
         if (value.startsWith("*")) {
+            System.out.println("-log参数值错误，退出程序");
             return false;
         }
         // -log的参数值是一个目录的路径
@@ -238,12 +247,13 @@ class OutArgument extends BaseArgument {
         if (strs.length > 2) {
             value = "*";
         }
-        value += strs[1];
+        value = strs[1];
     }
 
     boolean checkError() {
         // -out只有一个参数值
         if (value.startsWith("*")) {
+            System.out.println("-out参数值错误，退出程序");
             return false;
         }
         // 若输入的生成文件路径没有后缀 或后缀不为".txt" //则修改为默认
@@ -278,6 +288,7 @@ class DateArgument extends BaseArgument {
     boolean checkError() {
         // -date只有一个参数值
         if (value.startsWith("*")) {
+            System.out.println("-date参数值错误，退出程序");
             return false;
         }
         // 日期格式检查 满足平年闰年的YYYY-MM-DD格式
@@ -288,7 +299,7 @@ class DateArgument extends BaseArgument {
                         + "{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))"
                         + "|(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))"
                         + "-02-29) \r\n")) {
-            value = "";
+            value = "lastdate";
             System.out.println("日期格式有误，改为默认最新日期");
         }
         return true;
@@ -314,6 +325,7 @@ class TypeArgument extends BaseArgument {
             if (!(strs[i].equals("ip") || strs[i].equals("sp") || strs[i].equals("cure") || strs[i].equals("dead"))) {
                 strs[i] = "";
                 --n;
+                System.out.println("-type参数值只能为ip,sp,cure,dead，忽略该错误参数值");
             }
         }
         // 若全部输入错误 则视为参数缺省
@@ -321,7 +333,7 @@ class TypeArgument extends BaseArgument {
             valueList = new String[] { "ip", "sp", "cure", "dead" };
             return;
         }
-        String[] valueList = new String[n];
+        valueList = new String[n];
         for (int i = 0, j = 1; i < n; ++i) {
             while (strs[j].equals("")) {
                 ++j;
@@ -351,7 +363,7 @@ class TypeArgument extends BaseArgument {
 
 class ProvinceArgument extends BaseArgument {
 
-    String[] valuelist;
+    String[] valueList;
 
     ProvinceArgument(String name) {
         // TODO Auto-generated constructor stub
@@ -362,13 +374,25 @@ class ProvinceArgument extends BaseArgument {
         super(strs);
         // TODO Auto-generated constructor stub
         value = "valuelist";
-        valuelist = new String[strs.length - 1];
+        valueList = new String[strs.length - 1];
         for (int i = 1; i < strs.length; ++i) {
-            valuelist[i - 1] = strs[i];
+            valueList[i - 1] = strs[i];
         }
     }
 
-    boolean checkError() {
+    boolean checkError(){
+        return true;
+    }
+    
+    boolean checkError(Statistic sta) {
+        //输入的省份存在，若不存在则列出所有
+        for(int i=0;i<valueList.length;++i) {
+            if(!sta.data.containsKey(valueList[i])) {
+                value="all";
+                valueList=new String[0];
+                System.out.println("-province参数输入有误，默认列出所有省份数据");
+            }
+        }
         return true;
     }
 }

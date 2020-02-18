d56305435c33a47cc9d977711132d73a049dd368
KevinCai221701221 <57496940+KevinCai221701221@users.noreply.github.com>
2020-02-18 21:20:53 +0800

    secend
    set some paraments and methods

--- 221701221/src/InfectStatistic.java
+++ 221701221/src/InfectStatistic.java
@@ -2,12 +2,101 @@
  * InfectStatistic
  * TODO
  *
- * @author xxx
- * @version xxx
+ * @author Kvc
+ * @version 1.1.0
  * @since xxx
  */
+
+import java.io.*;
+import java.text.Collator;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static java.util.Map.Entry.comparingByValue;
+import static java.util.stream.Collectors.toMap;
 class InfectStatistic {
-    public static void main(String[] args) {
-        System.out.println("helloworld");
+    private CmdArgs cmdArgs;
+    private Map<String  , File> LogsMap;
+    private Container container;
+    private Record nation;
+
+
+    public CmdArgs GetCmdArgs()
+    {
+        return cmdArgs;
+    }
+
+    public Map<String , File> GetLogsMap()
+    {
+        return LogsMap;
+    }
+
+    public void SetLogsMap(Map<String , File> LogsMap)
+    {
+        this.LogsMap = LogsMap;
+    }
+
+    public Container GetContainer()
+    {
+        return container;
+    }
+
+    public void AddIpNum(Record record , int num)
+    {
+        record.UpIpNum(num);
+        nation.UpIpNum(num);
+    }
+
+    public void AddSpNum(Record record , int num)
+    {
+        record.UpSpNum(num);
+        nation.UpSpNum(num);
+    }
+
+    public void AddDeadNum(Record record , int num)
+    {
+        record.UpDeadNum(num);
+        record.DownIpNum(num);
+        nation.UpDeadNum(num);
+        nation.DownIpNum(num);
+    }
+
+    public void AddCureNum(Record record , int num)
+    {
+        record.UpCureNum(num);
+        record.DownIpNum(num);
+        nation.UpCureNum(num);
+        nation.DownIpNum(num);
+    }
+
+
+
+    public void Sp2Ip(Record record , int num)
+    {
+        record.UpIpNum(num);
+        record.DownSpNum(num);
+        nation.UpIpNum(num);
+        nation.DownSpNum(num);
+    }
+
+    public void ExcludeSp(Record record , int num)
+    {
+        record.DownSpNum(num);
+        nation.DownSpNum(num);
+    }
+
+    public void Move(Record FromRecord , Record InfectedRecord , String type , int num)
+    {
+        if (type.equals(Lib.Ip))
+        {
+            FromRecord.DownIpNum(num);
+            InfectedRecord.UpIpNum(num);
+        }
+        else if (type.equals(Lib.Sp))
+        {
+            FromRecord.DownSpNum(num);
+            InfectedRecord.UpSpNum(num);
+        }
     }
 }
--- 221701221/src/Lib.java
+++ 221701221/src/Lib.java
@@ -1,10 +1,24 @@
 /**
  * Lib
  * TODO
- *
- * @author xxx
- * @version xxx
+ * Common
+ * @author Kvc
+ * @version 1.1.0
  * @since xxx
  */
 public class Lib {
+    public static String logFileRegex = "2020-[0-1][0-9]-[0-3][0-9].log.txt";
+    public static String IP = "感染患者";
+    public static String Sp = "疑似患者";
+    public static String Cure = "治愈";
+    public static String Dead = "死亡";
+    public static String Exclude = "排除";
+    public static String Increase = "新增";
+    public static String National = "全国";
+    public static String Skip = "//";
+    public static String[] AllType = {"IP", "Sp", "Cure", "Dead"};
+    public static String[] Provinces = {"安徽", "北京","重庆","福建","甘肃","广东","广西","贵州",
+            "海南","河北","河南","黑龙江","湖北","湖南","吉林","江苏",
+            "江西","辽宁","内蒙古","宁夏","青海","山东","山西","陕西",
+            "上海","四川","天津","西藏","新疆","云南","浙江"};
 }

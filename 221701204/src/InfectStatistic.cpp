993648b7d2922f0d1f02d69d0d2caa8402506dfb
LOCICC <1150206533@qq.com>
2020-02-19 23:31:40 +0800

    ne

--- 221701204/src/InfectStatistic.cpp
+++ 221701204/src/InfectStatistic.cpp
@@ -1,4 +1,40 @@
-#include "path.h"
+
+#include <io.h>		
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <windows.h>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<string>
+
+using namespace std;
+
+class analysis
+{
+public:
+	char outpath[1005];
+	string province[40];
+	map<string, int>mi;
+	map<string, int>mi1;
+	map<string, int>mi2;
+	map<string, int>mi3;
+	string p[40];
+	string type[20];
+	int num, num1, opp;
+	int out[40][6];
+	int Num(string tmp);
+
+	void findAllFile(const char* path, const char* format, const char* date);
+	string begin(string com);
+	void init();
+	void Out();
+	int check;
+	int Detail(string p, string t) {
+		return out[mi[p]][mi2[t]];
+	}
+};
 
 int analysis::Num(string tmp)
 {
@@ -11,28 +47,36 @@ int analysis::Num(string tmp)
 }
 void analysis::findAllFile(const char* path, const char* format, const char* date)
 {
+	check = 1;
 	char newpath[200];
 	char newpaths[200];
-	strcpy_s(newpath, path);
-	strcpy_s(newpaths, path);
-	strcat_s(newpath, "\\*.*");    // 在目录后面加上"\\*.*"进行第一次搜索
+	strcpy(newpath, path);
+	strcpy(newpaths, path);
+	strcat(newpath, "\\*.*");    // 在目录后面加上"\\*.*"进行第一次搜索
 	int handle;
+	int b = 0;
 	_finddata_t findData;
+	check = 2;
 	handle = _findfirst(newpath, &findData);
-	if (handle == -1)        // 检查是否成功
+	if (handle == -1)
+	{
+		check = 3;
+		// 检查是否成功
 		return;
+	}
 	while (_findnext(handle, &findData) == 0) {
 		if (findData.attrib & _A_SUBDIR) {
 			if (strcmp(findData.name, ".") == 0 || strcmp(findData.name, "..") == 0)
 				continue;
-			strcpy_s(newpath, path);
-			strcat_s(newpath, "\\");
-			strcat_s(newpath, findData.name);
+			strcpy(newpath, path);
+			strcat(newpath, "\\");
+			strcat(newpath, findData.name);
 			findAllFile(newpath, format, date);
 		}
 		else {
 			if (strstr(findData.name, format))
 			{     //判断是不是txt文件 
+				
 				int size = strlen(date);
 				int fl = 1;
 				if (size != 0)
@@ -42,11 +86,12 @@ void analysis::findAllFile(const char* path, const char* format, const char* dat
 						if (findData.name[i] != date[i])fl = 0;
 					}
 				}
-				//	freopen(outpath, "w", stdout);
+				if (fl == 1)b = 1;
+				freopen(outpath, "w", stdout);
 				char nowpath[200];
-				strcpy_s(nowpath, newpaths);
-				strcat_s(nowpath, "\\");
-				strcat_s(nowpath, findData.name);
+				strcpy(nowpath, newpaths);
+				strcat(nowpath, "\\");
+				strcat(nowpath, findData.name);
 				ifstream fin(nowpath);
 				if (!fin.is_open())cout << "error";
 				string tmp;
@@ -90,7 +135,7 @@ void analysis::findAllFile(const char* path, const char* format, const char* dat
 						}
 						else ss = ss + tmp[j];
 					}
-
+					check = 1;
 					int num = Num(tmp);
 					//	cout<<fl1<<' '<<fl2<<' '<<f1<<' '<<f2<<' '<<num<<endl;
 					if (fl2 == 0 && f2 == 0)
@@ -102,8 +147,9 @@ void analysis::findAllFile(const char* path, const char* format, const char* dat
 					}
 					else if (fl2 == 0 && f2 != 0)out[fl1][mi2["疑似患者"]] -= num, out[fl1][mi2["感染患者"]] += num;
 					else out[fl1][f1] -= num, out[fl2][f1] += num;
+					if (out[fl1][f1] != 0)check = out[fl1][f1];
 				}
-				if (fl == 1)return;
+				if (b == 1)return;
 			}
 		}
 	}
@@ -123,21 +169,32 @@ void analysis::init()
 	mi["治愈"] = 102;
 	mi["死亡"] = 103;
 	mi["全国"] = 1;
-	//string s="北京市 天津市 上海市 重庆市 河北省 山西省 辽宁省 吉林省 黑龙江省 江苏省 浙江省 安徽省 福建省 江西省 山东省 河南省 湖北省 湖南省 广东省 海南省 四川省 贵州省 云南省 陕西省 甘肃省 青海省 台湾省 内蒙古自治区 广西壮族自治区 西藏自治区 宁夏回族自治区 新疆维吾尔自治区 香港特别行政区 澳门特别行政区";
-   // string s="安徽 北京 重庆 福建 甘肃 广东 广西 贵州 海南 河北 河南 黑龙江 湖北 湖南 吉林 江苏 江西 辽宁 内蒙古 宁夏 青海 山东 山西 陕西 上海  四川 台湾 天津 西藏 香港 新疆 云南省 浙江省 ";
-	mi["安徽"] = 2, mi["北京"] = 3, mi["重庆"] = 4, mi["福建"] = 5, mi["甘肃"] = 6, mi["广东"] = 7, mi["广西"] = 8, mi["贵州"] = 9, mi["海南"] = 10, mi["河北"] = 11, mi["河南"] = 12, mi["黑龙江"] = 13, mi["湖北"] = 14, mi["湖南"] = 15, mi["吉林"] = 16, mi["江苏"] = 17, mi["江西"] = 18, mi["辽宁"] = 19, mi["内蒙古"] = 20, mi["宁夏"] = 21, mi["青海"] = 22, mi["山东"] = 23, mi["山西"] = 24, mi["陕西"] = 25, mi["上海"] = 26, mi[""] = 27, mi["四川"] = 28, mi["台湾"] = 29, mi["天津"] = 30, mi["西藏"] = 31, mi["香港"] = 32, mi["新疆"] = 33, mi["云南"] = 34;
-	p[1] = "全国", p[2] = "安徽", p[3] = "北京", p[4] = "重庆", p[5] = "福建", p[6] = "甘肃", p[7] = "广东", p[8] = "广西", p[9] = "贵州", p[10] = "海南", p[11] = "河北", p[12] = "河南", p[13] = "黑龙江", p[14] = "湖北", p[15] = "湖南", p[16] = "吉林", p[17] = "江苏", p[18] = "江西", p[19] = "辽宁", p[20] = "内蒙古", p[21] = "宁夏", p[22] = "青海", p[23] = "山东", p[24] = "山西", p[25] = "陕西", p[26] = "上海", p[28] = "四川", p[29] = "台湾", p[30] = "天津", p[31] = "西藏", p[32] = "香港", p[33] = "新疆", p[34] = "云南";
+	mi["安徽"] = 2, mi["北京"] = 3, mi["重庆"] = 4, mi["福建"] = 5, mi["甘肃"] = 6, mi["广东"] = 7;
+	mi["广西"] = 8, mi["贵州"] = 9, mi["海南"] = 10, mi["河北"] = 11, mi["河南"] = 12, mi["黑龙江"] = 13;
+	mi["湖北"] = 14, mi["湖南"] = 15, mi["吉林"] = 16, mi["江苏"] = 17, mi["江西"] = 18, mi["辽宁"] = 19;
+	mi["内蒙古"] = 20, mi["宁夏"] = 21, mi["青海"] = 22, mi["山东"] = 23, mi["山西"] = 24, mi["陕西"] = 25;
+	mi["上海"] = 26, mi[""] = 27, mi["四川"] = 28, mi["台湾"] = 29, mi["天津"] = 30, mi["西藏"] = 31;
+	mi["香港"] = 32, mi["新疆"] = 33, mi["云南"] = 34,mi["浙江"];
+	p[1] = "全国", p[2] = "安徽", p[3] = "北京", p[4] = "重庆", p[5] = "福建", p[6] = "甘肃";
+	p[7] = "广东", p[8] = "广西", p[9] = "贵州", p[10] = "海南", p[11] = "河北", p[12] = "河南" ;
+	p[13] = "黑龙江", p[14] = "湖北", p[15] = "湖南", p[16] = "吉林", p[17] = "江苏", p[18] = "江西";
+	p[19] = "辽宁", p[20] = "内蒙古", p[21] = "宁夏", p[22] = "青海", p[23] = "山东", p[24] = "山西";
+	p[25] = "陕西", p[26] = "上海", p[28] = "四川", p[29] = "台湾", p[30] = "天津", p[31] = "西藏";
+	p[32] = "香港", p[33] = "新疆", p[34] = "云南",p[35]="浙江";
 }
-void analysis::Out()
+
+
+void  analysis::Out()
 {
-	for (int i = 2;i < 35;i++)
+	int o = 0;
+	for (int i = 2;i < 36;i++)
 	{
 		for (int j = 1;j < num1;j++)
 		{
 			out[1][j] += out[i][j];
 		}
 	}
-	for (int i = 1;i < 35;i++)
+	for (int i = 1;i < 36;i++)
 	{
 		if (mi1.count(p[i]) || (num == 0 && mi3.count(p[i])) || i == 1)
 		{
@@ -145,18 +202,21 @@ void analysis::Out()
 			for (int j = 1;j < num1;j++)
 			{
 				cout << type[j] << out[i][j] << "人 ";
+				if (out[i][j]!=0)check = out[i][j];
 			}
 			if (mi1[p[i]] == 1 || num == 0)
 			{
-				cout << endl;
+				cout << "\n";
 			}
 		}
 	}
+	cout << "// 该文档并非真实数据，仅供测试使用" << "\n";
 }
 string analysis::begin(string com)
 {
 	init();
 	char inpath[1005], date[105];
+	
 	int cc = 0;
 	int size = com.size();
 	for (int i = 0;i < size;i++)
@@ -178,7 +238,7 @@ string analysis::begin(string com)
 			}
 			inpath[c] = '\0';
 		}
-		if (s == "-out")
+		else if (s == "-out")
 		{
 			int c = 0;
 			while (i < size && com[i] != ' ')
@@ -188,7 +248,7 @@ string analysis::begin(string com)
 			}
 			outpath[c] = '\0';
 		}
-		if (s == "-date")
+		else if (s == "-date")
 		{
 			int c = 0;
 			while (i < size && com[i] != ' ')
@@ -199,7 +259,7 @@ string analysis::begin(string com)
 			cc = c;
 			date[c] = '\0';
 		}
-		if (s == "-type")
+		else if (s == "-type")
 		{
 			int c = 1;
 			string ss = "";
@@ -224,7 +284,7 @@ string analysis::begin(string com)
 			if (!mi2.count("死亡"))mi2["死亡"] = c, type[c++] = "死亡";
 			mi2["确诊感染"] = 11;
 		}
-		if (s == "-province")
+		else if (s == "-province")
 		{
 			int c = 0;
 			while (i < size && (com[i] >= 'z' || com[i] <= 'a') && com[i] != '-')
@@ -237,6 +297,8 @@ string analysis::begin(string com)
 			if (com[i] >= 'z' || com[i] <= 'a')i--;
 			num = c;
 		}
+		else if(com[i]==' ');
+		else i--;
 	}
 	if (num1 == 0)
 	{
@@ -250,8 +312,6 @@ string analysis::begin(string com)
 	}
 	
 	findAllFile(inpath, ".log.txt", date);
-	Out();
-
 	string o = "";
 	if (cc > 0)
 	{
@@ -268,7 +328,6 @@ string analysis::begin(string com)
 		for (int i = 1;i < num1;i++)o = o + type[i];
 		o = o + " ";
 	}
-	cout << o << "c";
 	return o;
 
 }
@@ -278,7 +337,7 @@ int main()
 {
 	analysis A;
 	char com[1005];
-	gets_s(com);
+	gets(com);
 	string s = "";
 	int size = strlen(com);
 	for (int i = 0;i < size;i++)
@@ -286,6 +345,9 @@ int main()
 		s = s + com[i];
 	}
 	A.begin(s);
+	A.Out();
+	
 	return 0;
 }
+
 

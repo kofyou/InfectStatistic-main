f7c9e4ce03e8fa549ec855955db4700ef03b9f79
xubanxian233 <52060081+xubanxian233@users.noreply.github.com>
2020-02-20 17:22:33 +0800

    读取日志文件并处理数据，保存至hashmap中

--- 221701321/src/InfectStatistic.java
+++ 221701321/src/InfectStatistic.java
@@ -1,12 +1,14 @@
 import javax.naming.CompoundName;
 import javax.naming.directory.Attributes;
 import javax.xml.xpath.XPath;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.security.Key;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -33,13 +35,26 @@ class InfectStatistic {
 
     static HashMap<String,String> list = new HashMap<>();
     private static String [][] commandlist ={{"-log",Inpath},{"-out",Outpath},{"-date",dateflag},{"-type",typeflag},{"-province",proflag}}; //命令集
-    private static ArrayList<String> provincelist = new ArrayList<String>(); //需要的的省份集
+    private static  ArrayList<String> provincelist = new ArrayList<String>();//需要的的省份集
+    private static HashMap<String,Integer[]> prolist = new HashMap<String,Integer[]>(); //处理的的的省份集
     private static  ArrayList<String> typelist = new ArrayList<String>(); //需要的状态集,
     private static ArrayList<String> datelist = new ArrayList<String>(); //已处理的日志时间集
-    private  static  String[] need = new String[]{"0","0","0"};
+    private static ArrayList<String> dlist = new ArrayList<>(); //符合格式的日志文件名列表
+    private  static  String[] need = new String[]{"0","0","0"}; //三个必要参数是否存在
     private static String[] province = new String[]{"全国","安徽","北京","重庆","福建","甘肃","广东","广西","贵州","海南","河北","河南","黑龙江","湖北","湖南","吉林",
             "江苏","江西","辽宁","内蒙古","宁夏","青海","山东","山西","陕西","上海","四川","天津","西藏","新疆","云南","浙江"};//省份集
     private static String[] type = new String[]{"ip","sp","cure","dead"};//状态集，{"ip","感染患者"},{"sp","疑似患者"},{"cure","治愈患者"},{"dead","死亡患者"}
+    private static String[] status = new String[]{"新增 感染患者","新增 疑似患者","感染患者 流入","疑似患者 流入","死亡","治愈","疑似患者 确诊感染","排除 疑似患者"};//每日情况
+    /*
+    1、<省> 新增 感染患者 n人
+2、<省> 新增 疑似患者 n人
+3、<省1> 感染患者 流入 <省2> n人
+4、<省1> 疑似患者 流入 <省2> n人
+5、<省> 死亡 n人
+6、<省> 治愈 n人
+7、<省> 疑似患者 确诊感染 n人
+8、<省> 排除 疑似患者 n人
+     */
 
     /*处理传递进来的参数*/
     public static boolean InputLog(String[] Mes)
@@ -163,6 +178,15 @@ class InfectStatistic {
                 flag = flag;
             }
         }
+        if(proflag.equals("0")){
+            provincelist.add("全国");
+        }
+        if(typeflag.equals("0")){
+            typelist.add(type[0]);
+            typelist.add(type[1]);
+            typelist.add(type[2]);
+            typelist.add(type[3]);
+        }
         return flag;
     }
 
@@ -181,7 +205,7 @@ class InfectStatistic {
     public static void GetPathandName(String text)
     {
         Outpath = text.substring(0,text.lastIndexOf("/")+1);
-        filename = text.substring(text.lastIndexOf("/")+1);
+        filename = text.substring(text.lastIndexOf("/")+1,text.lastIndexOf("."));
         named = text.substring(text.lastIndexOf(".")+1);
     }
 
@@ -204,25 +228,187 @@ class InfectStatistic {
         return a;
     }
 
+    public static boolean ReadLog(){
+        int count = 0;
+        try {
+            File file = new File(Inpath);
+            if (!file.isDirectory()) {
+                System.out.println("文件");
+                System.out.println("path=" + file.getPath());
+                System.out.println("absolutepath=" + file.getAbsolutePath());
+                System.out.println("name=" + file.getName());
+
+            } else if (file.isDirectory()) {
+                String[] filelist = file.list();
+                String pattern = "[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9].log.txt";
+                Pattern r = Pattern.compile(pattern);
+                for (int i = 0; i < filelist.length; i++) {
+                    File readfile = new File(Inpath + "\\" + filelist[i]);
+                    Matcher m = r.matcher(readfile.getName());
+                    if(m.find()){
+                        count++;
+                        dlist.add(readfile.getName());
+                    }else{
+                        continue;
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("readfile()   Exception:" + e.getMessage());
+        }
+        boolean a = true;
+        if(count==0){
+            a = false;
+
+        }else {
+            a=true;
+        }
+        return a;
+    }
+
+    public static void ReadData(){
+        String path;
+        Integer[] m1,m2;
+        for(String n:dlist){
+            if(n.compareTo(date) < 0) {
+                path = Inpath + n;
+                try (FileReader reader = new FileReader(path);
+                     BufferedReader br = new BufferedReader(reader) // 建立一个对象，它把文件内容转成计算机能读懂的语言
+                ) {
+                    String line;
+                    //网友推荐更加简洁的写法
+                    while ((line = br.readLine()) != null && (!line.startsWith("//"))) {
+                        // 一次读入一行数据
+                        for (int i = 0; i < status.length; i++) {
+                            if (line.contains(status[i])) {
+                                String[] lined = line.split("\\s+");
+                                String str = "";
+                                String str1 = lined[lined.length - 1];
+                                for (int j = 0; j < str1.length(); j++) {
+                                    if (str1.charAt(j) >= 48 && str1.charAt(j) <= 57) {
+                                        str += str1.charAt(j);
+                                    }
+                                }
+                                str = str.trim();
+                                Integer num = 0;
+                                num = Integer.parseInt(str);
+                                switch (i) {
+                                    case 0: {
+                                        if (prolist.containsKey(lined[0])) {
+                                            m1 = prolist.get(lined[0]);
+                                            m1[0] += num;
+                                            prolist.put(lined[0], m1);
+                                        }
+                                        break;
+                                    }
+                                    case 1: {
+                                        if (prolist.containsKey(lined[0])) {
+                                            m1 = prolist.get(lined[0]);
+                                            m1[1] += num;
+                                            prolist.put(lined[0], m1);
+                                        }
+                                        break;
+                                    }
+                                    case 2: {
+                                        if (prolist.containsKey(lined[0]) && prolist.containsKey(lined[3])) {
+                                            m1 = prolist.get(lined[0]);
+                                            m2 = prolist.get(lined[3]);
+                                            m1[0] -= num;
+                                            m2[0] += num;
+                                            prolist.put(lined[0], m1);
+                                            prolist.put(lined[3], m2);
+                                        }
+                                        break;
+                                    }
+                                    case 3: {
+                                        if (prolist.containsKey(lined[0]) && prolist.containsKey(lined[3])) {
+                                            m1 = prolist.get(lined[0]);
+                                            m2 = prolist.get(lined[3]);
+                                            m1[1] -= num;
+                                            m2[1] += num;
+                                            prolist.put(lined[0], m1);
+                                            prolist.put(lined[3], m2);
+                                        }
+                                        break;
+                                    }
+                                    case 4: {
+                                        if (prolist.containsKey(lined[0])) {
+                                            m1 = prolist.get(lined[0]);
+                                            m1[0] -= num;
+                                            m1[3] += num;
+                                            prolist.put(lined[0], m1);
+                                        }
+                                        break;
+                                    }
+                                    case 5: {
+                                        if (prolist.containsKey(lined[0])) {
+                                            m1 = prolist.get(lined[0]);
+                                            m1[0] -= num;
+                                            m1[2] += num;
+                                            prolist.put(lined[0], m1);
+                                        }
+                                        break;
+                                    }
+                                    case 6: {
+                                        if (prolist.containsKey(lined[0])) {
+                                            m1 = prolist.get(lined[0]);
+                                            m1[1] -= num;
+                                            m1[0] += num;
+                                            prolist.put(lined[0], m1);
+                                        }
+                                        break;
+                                    }
+                                    case 7: {
+                                        if (prolist.containsKey(lined[0])) {
+                                            m1 = prolist.get(lined[0]);
+                                            m1[1] -= num;
+                                            prolist.put(lined[0], m1);
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+
+                        }
+
+                        System.out.println(line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args)
     {//java InfectStatistic list -date 2020-01-22 -log D:/log/ -out D:/output.txt
-
-        list.put("-log",Inpath);
-        args= new String[]{ "list","-date","2020-01-39","-log","D:/log/","-out","D:/output.txt","-type","ip","ap"};
+        Integer[] inte = new Integer[]{0,0,0,0};
+        for(String p:province){
+            prolist.put(p,new Integer[]{0,0,0,0});
+        }
+        args= new String[]{ "list","-date","2019-02-30","-log","D:/log/","-out","D:/output.txt","-type","ip","sp"};
 
         if(InputLog(args)){
-            for (HashMap.Entry<String, String> entry : list.entrySet()) {
-                String key = entry.getKey();
-                String val = entry.getValue();
-                System.out.println(key);
-                System.out.println(val);
+            if(ReadLog()) {
+                Collections.sort(dlist);
+                if(dateflag.equals("0")){
+                    date=dlist.get(dlist.size());
+                }
+                ReadData();
+                for (HashMap.Entry<String, Integer[]> entry : prolist.entrySet()) {
+                    String key = entry.getKey();
+                    Integer[] val = entry.getValue();
+                    System.out.println(key);
+                    for(int i:val){
+                        System.out.println(i);
+                    }
+                }
+            }else {
+                System.out.println("无日志文件！");
             }
-            System.out.println(Outpath);
-            System.out.println(Inpath);
-            System.out.println(filename);
-            System.out.println(date);
-            System.out.println(provincelist);
-            System.out.println(typelist);
         }else{
             System.out.println("无法执行！");
         }

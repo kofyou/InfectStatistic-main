17ba163f0c63d5f6909453086c7f4fd9c436eeab
ll1998 <61130424+ll1998@users.noreply.github.com>
2020-02-20 22:49:15 +0800

    fix:writeFile,dealCmd again

--- example/src/InfectStatistic.java
+++ example/src/InfectStatistic.java
@@ -251,7 +251,7 @@ class InfectStatistic {
     }
 
     //写文件
-    public void writeFile(String path,Hashtable<String,Province> proHash,String[] type,String[] province) throws IOException {
+    public void writeFile(String path,Hashtable<String,Province> proHash,Hashtable<Integer,String> typeHash,String[] province) throws IOException {
     	
     	String[] sortedPro = {"全国","安徽","北京","重庆","福建","甘肃","广东","广西","贵州","海南","河北","河南","黑龙江","湖北","湖南","吉林",
     			"江苏","江西","辽宁","内蒙古","宁夏","青海","山东","山西","陕西","上海","四川","天津","西藏","新疆","云南","浙江"};
@@ -259,75 +259,133 @@ class InfectStatistic {
     	String content=null;
 		FileWriter fileWriter;
 		BufferedWriter bw;
-		File file = new File(path);
+		
 		
 		//-province
 		if(province==null) {//不指定省份
-			//System.out.println("全国 "+"感染患者"+proHash.get("全国").getIp()+"人 疑似患者 "+proHash.get("全国").getSp()+"人 治愈 "+proHash.get("全国").getCure()+"人 死亡 "+proHash.get("全国").getDead()+"人");
-			System.out.println("province is null");
+			File file = new File(path);
+	        file.createNewFile();
+	        fileWriter = new FileWriter(file.getAbsoluteFile(),true);
+			bw = new BufferedWriter(fileWriter);
 			try {	
-				if(!file.exists()){
-					file.createNewFile();
-				}
 				for(int i=0;i<32;i++) {
 					if(proHash.containsKey(sortedPro[i])) {			
 						String keys=sortedPro[i];
-						//System.out.println(keys);
-						content=proHash.get(keys).getName()+" 感染患者"+proHash.get(keys).getIp()+"人 疑似患者"+proHash.get(keys).getSp()+"人 治愈"+proHash.get(keys).getCure()+"人 死亡"+proHash.get(keys).getDead()+"人 \n";
-						fileWriter = new FileWriter(file.getAbsoluteFile(),true);
-						bw = new BufferedWriter(fileWriter);
+						if(typeHash!=null) {
+							content=keys;
+							for(int k=0;k<typeHash.size();k++) {
+					            if(typeHash.containsKey(k)) {
+					           		String s=typeHash.get(k);
+					           		switch(s) {
+					           			case "ip":
+					           				content+=" 感染患者"+proHash.get(keys).getIp()+"人";
+					           				break;
+					           			case "sp":
+					           				content+=" 疑似患者"+proHash.get(keys).getSp()+"人";
+					           				break;
+					           			case "cure":
+					           				content+=" 治愈"+proHash.get(keys).getCure()+"人";
+					           				break;
+					           			case "dead":
+					           				content+=" 死亡"+proHash.get(keys).getDead()+"人";
+					           				break;
+				            		}
+				            	}
+				            }
+							content+="\n";
+						}
+						else
+							content=proHash.get(keys).getName()+" 感染患者"+proHash.get(keys).getIp()+"人 疑似患者"+proHash.get(keys).getSp()+"人 治愈"+proHash.get(keys).getCure()+"人 死亡"+proHash.get(keys).getDead()+"人 \n";
+						
+						
 						bw.write(content);
-						//System.out.println(content);
-						bw.close();
+						
 					}
 				}
-				
-				/*Set set = proHash.keySet();
-		        Iterator iterator = set.iterator();
-		        while(iterator.hasNext()) {
-		            Object keys = iterator.next();
-		            
-					
-		        }*/
-		    			    	
-				//content = "全国 "+"感染患者"+proHash.get("全国").getIp()+"人 疑似患者 "+proHash.get("全国").getSp()+"人 治愈 "+proHash.get("全国").getCure()+"人 死亡 "+proHash.get("全国").getDead()+"人\n";
+				bw.write("// 该文档并非真实数据，仅供测试使用\n");
+				bw.close();
 		    } 
 			catch (IOException e) {
 		        e.printStackTrace();
 		    }
 		}
-		else{//有指定省份
+		else if(province!=null){//有指定省份
+			File file = new File(path);
 			for(int i=0;i<32;i++) {
-				for(int j=0;i<province.length;j++) {
-					if(proHash.containsKey(province[j])&&province[j]==sortedPro[i]) {
+				for(int j=0;j<province.length;j++) {
+					if(proHash.containsKey(province[j])&&province[j]==sortedPro[i]&&!province[j].equals("无")) {//哈希表中有此省份
 						String keys=sortedPro[i];
-						System.out.println(keys);
-						
-						content=keys;
-						
-						for(int k=0;k<type.length;k++) {
-							
-							content+=type[k];
-							System.out.println("***********");
+						if(typeHash!=null) {
+							content=keys;
+							for(int k=0;k<typeHash.size();k++) {
+				            
+				            	if(typeHash.containsKey(k)) {
+				            		String s=typeHash.get(k);
+				            
+				            		switch(s) {
+				            			case "ip":
+				            				content+=" 感染患者"+proHash.get(keys).getIp()+"人";
+				            				break;
+				            			case "sp":
+				            				content+=" 疑似患者"+proHash.get(keys).getSp()+"人";
+				            				break;
+				            			case "cure":
+				            				content+=" 治愈"+proHash.get(keys).getCure()+"人";
+				            				break;
+				            			case "dead":
+				            				content+=" 死亡"+proHash.get(keys).getDead()+"人";
+				            				break;
+				            		}
+				            	}
+				            }
+							content+="\n";
 						}
-						content+="人\n";
-						System.out.println(content);
-						//content=proHash.get(keys).getName()+" 感染患者"+proHash.get(keys).getIp()+"人 疑似患者"+proHash.get(keys).getSp()+"人 治愈"+proHash.get(keys).getCure()+"人 死亡"+proHash.get(keys).getDead()+"人 \n";
+						else
+							content=proHash.get(keys).getName()+" 感染患者"+proHash.get(keys).getIp()+"人 疑似患者"+proHash.get(keys).getSp()+"人 治愈"+proHash.get(keys).getCure()+"人 死亡"+proHash.get(keys).getDead()+"人 \n";
+						
 						fileWriter = new FileWriter(file.getAbsoluteFile(),true);
 						bw = new BufferedWriter(fileWriter);
 						bw.write(content);
 						bw.close();
 					}
-					else if(!proHash.containsKey(province[j])&&province[j]==sortedPro[i]){
+					else if(!proHash.containsKey(province[j])&&province[j]==sortedPro[i]){//哈希表中没有这个省份
 						String keys=sortedPro[i];
-						content=proHash.get(keys).getName()+" 感染患者0人 疑似患者0人 治愈0人 死亡0人\n";
+						content=keys;
+						if(typeHash!=null) {
+							for(int k=0;k<typeHash.size();k++) {
+				            	if(typeHash.containsKey(k)) {
+				            		String s=typeHash.get(k);
+				            		switch(s) {
+				            			case "ip":
+				            				content+=" 感染患者0人";
+				            				break;
+				            			case "sp":
+				            				content+=" 疑似患者0人";
+				            				break;
+				            			case "cure":
+				            				content+=" 治愈0人";
+				            				break;
+				            			case "dead":
+				            				content+=" 死亡0人";
+				            				break;
+				            		}
+				            	}
+				            }
+							content+="\n";
+						}
+						else
+							content=sortedPro[i]+" 感染患者0人 疑似患者0人 治愈0人 死亡0人\n";
+						fileWriter = new FileWriter(file.getAbsoluteFile(),true);
+						bw = new BufferedWriter(fileWriter);
+						content+="// 该文档并非真实数据，仅供测试使用\n";
+						bw.write(content);	
+						bw.close();
 					}
+					
 				}
 			}
 			
 		}
-		
-		
 
     }
 
@@ -343,7 +401,6 @@ class InfectStatistic {
             nation.sp += hashtable.get(keys).getSp();
             nation.cure += hashtable.get(keys).getCure();
             nation.dead += hashtable.get(keys).getDead();
-            //System.out.println(hashtable.get(keys).getName()+hashtable.get(keys).getIp()+" "+hashtable.get(keys).getSp()+" "+hashtable.get(keys).getCure()+hashtable.get(keys).getDead());
         }
     	hashtable.put("全国",nation);
     	
@@ -357,7 +414,7 @@ class InfectStatistic {
     //处理日期对象，比较日期大小，判断哪些文件需要处理
     private void dealDate(String filepath,int deep,String date,Hashtable<String,Province> hashtable,boolean flag) throws IOException{   
     	// 获得指定文件对象  
-        File file = new File(filepath);   
+        File file = new File(filepath);  
         // 获得该文件夹内的所有文件   
         File[] childFile = file.listFiles();  
 
@@ -374,7 +431,6 @@ class InfectStatistic {
                 //比较日期大小
                 String fileName=childFile[i].getName().substring(0,10);
                 if(date.compareTo(fileName)>=0) {
-                	//System.out.println(childFile[i].getPath());
                 	dealStatistic(childFile[i].getPath(),hashtable);     
                 }
                 
@@ -385,7 +441,7 @@ class InfectStatistic {
     //解析命令行
     public void dealCmd(String[] test,Hashtable<String,Province> proHash) throws IOException {
     	Hashtable<String,Integer> cmdHash=new Hashtable<String,Integer>();
-    	String[] type=new String[4];
+    	Hashtable<Integer,String> typeHash=new Hashtable<Integer,String>();
     	String[] province=new String[32];
         for(int i=0;i<test.length;i++) {
         	cmdHash.put(test[i], i);
@@ -407,73 +463,60 @@ class InfectStatistic {
             dealDate(test[cmdHash.get("-log")+1],0,currentDate,proHash,flagDate); 
         }
         Sum(proHash);
-        System.out.println("ip:"+proHash.get("全国").ip+"   sp:"+proHash.get("全国").sp+"  cure:"+proHash.get("全国").cure+"  dead:"+proHash.get("全国").dead);
+        //System.out.println("ip:"+proHash.get("全国").ip+"   sp:"+proHash.get("全国").sp+"  cure:"+proHash.get("全国").cure+"  dead:"+proHash.get("全国").dead);
         
         //处理-province
         if(cmdHash.containsKey("-province")) {
+        	int count=0;
+        	int i;
         	int num=cmdHash.get("-province")+1;
-        	for(int i=num,j=0;i<test.length;i++,j++) {
-        		if(test[num].substring(0,1).equals("-")) {
-        			break;
-        		}
-        		province[j]=test[num];
+        	for(i=num;i<test.length;i++) {
+                
+        		if(test[i].equals("-")) {
+        			count=i-num+1;
+        	   		break;
+        		}        		
+        	}
+        	if(count==0&&i==test.length) {//如果-province后面没有别的指令
+        		count=i-num;
+        	}
+      
+        	for(int j=0;j<province.length;j++) {
+        		if(num+j<test.length)
+        			province[j]=test[num+j];
+        		else 
+        			province[j]="无";
         		
         	}
+        	
         	
         }
         else
         	province=null;
         
         //处理-type
-        if(cmdHash.containsKey("-type")) {
-        	if(cmdHash.containsKey("ip")){
-            	type[0]="\" 感染患者\"+proHash.get(keys).getIp()";
-            }
-            else if(cmdHash.containsKey("sp")) {
-            	type[1]="\"人 疑似患者\"+proHash.get(keys).getSp()";
-            }
-            else if(cmdHash.containsKey("cure")) {
-            	type[2]="\"人 治愈\"+proHash.get(keys).getCure()";
-            }
-            else if(cmdHash.containsKey("dead")) {
-            	type[3]="\"人 死亡\"+proHash.get(keys).getDead()";
-            }   
+        if(cmdHash.containsKey("-type")) {	
+        	int num=cmdHash.get("-type")+1;
+        	for(int i=num;i<test.length;i++) {
+        		if(!test[i].substring(0,1).equals("-")) {
+        			typeHash.put(i-num,test[i]);
+        		}
+        	}
         }
         else
-        	type=null;
-        
-        writeFile("F:/output.txt",proHash,type,province);
+        	typeHash=null;
+       
+        writeFile("F:/output.txt",proHash,typeHash,province);
     }
     
     public static void main(String[] args) throws IOException {
         
         InfectStatistic in = new InfectStatistic();
-        
-        String[] test= {"list","-date","2020-01-22","-log","F:\\InfectStatistic-main\\example\\log\\","-out","D:/output.txt","-type","ip","-province","福建","湖北"};
-        
-        
-        //args[1] -datae    args[2] xxxx-xx-xx  args[3] -log args[4] xxx args[5] -out  args[6] xxx
+        //测试
+        String[] test= {"list","-date","2020-01-22","-log","F:\\InfectStatistic-main\\example\\log\\","-out","D:/output.txt","-type","sp","dead","-province","福建","江苏"};
         Hashtable<String,Province> proHash = new Hashtable<String, Province>();//以省份汉字作为Key 以省类作为value
         in.dealCmd(test, proHash);
-        
-        
-        
-        //这是需要获取的文件夹路径  
-      //  String path ="F:\\InfectStatistic-main\\example\\log";   
-          
-        
-        
-        
-        //in.dealStatistic("F:\\InfectStatistic-main\\example\\log\\2020-01-23.log.txt",proHash);
-        
-        
-       /* for(int i=0;i<test.length;i++) {
-        	System.out.println(test[i]);
-        }*/
-        
-      
-        
-      
+  
     }
     
 }

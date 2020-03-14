1be4033a658e6edbc35f1e2639cb6f973d165401
yz990219 <50985454+yz990219@users.noreply.github.com>
2020-03-14 13:34:45 +0800

    commit_last

---
+++ 221701430/src/InfectStatisticTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InfectStatisticTest {
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
